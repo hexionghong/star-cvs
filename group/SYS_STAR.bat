6d503db36c3d9317711c7cfd87f94ac49ee29f6a
cvs2git <>
1998-05-08 20:58:36 +0000

    This commit was manufactured by cvs2svn to create tag 'SL98b_2'.

--- group/SYS_STAR.bat
+++
@@ -1,7 +0,0 @@
-rem # Determine SYS_STAR variable.
-rem
-set SYS_STAR=intel_wnt
-echo "SYS_STAR"="%SYS_STAR%">>Env.reg
-
-set SYS_HOST_STAR=%SYS_STAR%
-echo "SYS_HOST_STAR"="%SYS_HOST_STAR%">>Env.reg
