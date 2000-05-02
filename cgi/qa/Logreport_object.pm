ea65950061afb09dec05433177732da607513ea0
bum <>
2000-05-02 22:58:19 +0000

    fixed log

--- cgi/qa/Logreport_object.pm
+++ cgi/qa/Logreport_object.pm
@@ -359,7 +359,7 @@ sub ParseLogfile {
     next unless $line =~ /^QAInfo:|^StWarning:|^StError/;
     
     # get STAR and ROOT levels
-    $line =~ /STAR_LEVEL :\W+(\w+), ROOT_LEVEL :\W+([0-9\.]+)/ and do{
+    $line =~ /STAR_LEVEL\W+(\w+), ROOT_LEVEL\W+([0-9\.]+)/ and do{
       $self->StarLevel($1);
       $self->RootLevel($2);
       next;
@@ -649,7 +649,7 @@ sub LogfileSummaryString {
 
   $return_string = "";
 
-  if ( $self->StarLevel() ) {
+  if ( $self->StarLevel() or $self->StarlibVersion()) {
 
     if ( $self->RunCompletedOk() ) {
       $return_string .= "run completed;";
