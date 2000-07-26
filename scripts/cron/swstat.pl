ad273b26af65c588008dbbc78dcba8224a243adf
wenaus <>
2000-07-26 14:41:57 +0000

    duvall management cron job

--- scripts/cron/swstat.pl
+++ scripts/cron/swstat.pl
@@ -1,8 +1,11 @@
 #!/opt/star/bin/perl
 #
-# $Id: swstat.pl,v 1.2 1999/11/22 18:18:30 wenaus Exp $
+# $Id: swstat.pl,v 1.3 2000/07/26 14:41:57 wenaus Exp $
 #
 # $Log: swstat.pl,v $
+# Revision 1.3  2000/07/26 14:41:57  wenaus
+# duvall management cron job
+#
 # Revision 1.2  1999/11/22 18:18:30  wenaus
 # Clean up
 #
@@ -30,19 +33,31 @@ my %rcfNodes = (
                 'rcasN' => 0,
                 'rcf' => 0,
                 'rcrsN' => 0,
+                'rcrsuser' => 0,
                 'rlnx01' => 0,
                 'rlnx02' => 0,
                 'rlnx03' => 0,
                 'rlnxsp' => 0,
+                'rlnxasis' => 0,
+                'rsunasis' => 0,
                 'rmds00' => 0,
                 'rmds03' => 0,
+                'rmine02' => 0,
+                'rmine03' => 0,
                 'rsgiN' => 0,
                 'rsun00' => 0,
                 'atlas00' => 0,
                 'play1' => 0,
                 'play2' => 0,
+                'play3' => 0,
+                'play7' => 0,
+                'play8' => 0,
+                'play10' => 0,
+                'play11' => 0,
+                'play14' => 0,
                 'rdev' => 0,
-                'www2' => 0
+                'www2' => 0,
+                'rftpexp' => 0
                 );
 my %starSunNodes = (
                     'sol' => 0,
@@ -56,51 +71,75 @@ my %starLinuxNodes = (
                       'robinson' => 0,
                       'pickford' => 0,
                       'midler' => 0,
-                      'connery' => 0
+                      'connery' => 0,
+                      'onllinux1' => 0,
+                      'onllinux2' => 0,
+                      'onllinux3' => 0,
+                      'onllinux4' => 0
                       );
 my %deskNodes = (
                  'vega' => 0,
                  'elvis' => 0,
                  'bogart' => 0,
                  'monroe' => 0,
+                 'lemmon' => 0,
+                 'sellers' => 0,
                  'soliton' => 0,
                  'astaire' => 0,
                  'monet' => 0,
                  'io' => 0,
                  'galaxy' => 0,
                  'charm' => 0,
-                 'richpc' => 0
+                 'richpc' => 0,
+                 'depardieu' => 0,
+                 'streisand' => 0,
+                 'rigel' => 0
                  );
 my %starHpNodes = (
-                   'flynn' => 0
+                   'flynn' => 0,
+                   'hpplus01' => 0,
+                   'hpplus03' => 0
                    );
+
+my %usatlasNodes = (
+                    'linuxN' => 0
+                    );
 
 my %starLblNodes = (
                     'starlxN' => 0,
                     'pdsflxN' => 0,
                     'starsu00' => 0,
+                    'pdsfsu00' => 0,
                     'starlbl5' => 0,
                     'rncsv1' => 0,
                     'rncus1' => 0,
-                    'rncsmb' => 0
+                    'rncus3' => 0,
+                    'rncsmb' => 0,
+                    'rnchp3' => 0,
+                    'sseos' => 0
                     );                 
 my %starRiceNodes = (
                      'bonner-pcsN' => 0,
+                     'bonner-pc19' => 0,
                      'bonner-lap1' => 0
                      );
 my %starIndianaNodes = (
+                        'pc-star1' => 0,
+                        'pc-star2' => 0,
+                        'pc-star3' => 0,
                         'pc-star4' => 0,
                         'pc-star5' => 0
                         );
 my $versions = `cd /afs/rhic/star/packages; ls -1 | grep SL`;
 $versions =~ s/\n/ /g;
+$versions .= "DEV00 .DEV";
 #@statfiles = ( 'SL99b', 'SL99d', 'SL99e', 'SL99f', 'SL99g', 'SL99h');
 my @statfiles = split (/ /,$versions);
 my %stattypes = ( 'star' => 'env',
                   'root4star' => 'root'
                   );
 my %levels, my %versions, my %users, my %hosts, my %userlev, my %hostLoc;
-my $rcfNodeCount, my $starSunNodeCount, my $starLinuxNodeCount, my $starHpNodeCount,
+my $rcfNodeCount, my $starSunNodeCount, my $starLinuxNodeCount, my $starHpNodeCount, my $usatlasNodeCount,
     my $starLblNodeCount, my $deskNodeCount, my $starRiceNodeCount, my $starIndianaNodeCount;
 foreach my $type ( sort keys %stattypes ) {
     foreach my $e ( keys %starHpNodes ) { $starHpNodes{$e} = 0; }
@@ -117,15 +156,19 @@ foreach my $type ( sort keys %stattypes ) {
                 split / /;
             if ( ! ($user =~ m/^[a-z]+/) ) {next}  # a few with junk characters
             $level =~ s/STAR_LEVEL=//;
-            if ( $level ne '.dev' && $level ne 'dev' && $level ne 'new' && $level ne 'pro' && $level ne 'old' && substr($level,0,2) ne 'SL' ) {next}
-            $ver =~ s/STAR_VERSION=//;
+            if ( $level ne '.dev' && $level ne 'dev' && $level ne 'new' && $level ne 'pro' && $level ne 'old' && substr($level,0,2) ne 'SL' && $level ne 'DEV00' && $level ne '.DEV' ) {next}
+            $ver =~ s/STAR_V[A-Z]+N=//;
             $host =~ s/\..*//;
             $host =~ s/rcrs[0-9]+/rcrsN/;
+            $host =~ s/^linux[0-9][0-9][0-9]/linuxN/;
             $host =~ s/rsgi[0-9]+/rsgiN/;
             $host =~ s/rcas[0-9]+/rcasN/;
             $host =~ s/starlx[0-9]+/starlxN/;
             $host =~ s/pdsflx[0-9]+/pdsflxN/;
             $host =~ s/bonner-pcs[0-9]+/bonner-pcsN/;
+            if ($level eq '.DEV' ) {$level = '.dev'}
+            if ($level eq 'DEV00' ) {$level = 'dev'}
+            if ($level eq 'DEV' ) {$level = 'dev'}
             if (substr($level,0,2) ne 'SL') {$levels{$level}++}
             $versions{$ver}++;
             $users{$user}++;
@@ -137,6 +180,7 @@ foreach my $type ( sort keys %stattypes ) {
             if ( exists( $starSunNodes{$host} ) ) {$starSunNodeCount++,$hostLoc{$host}='BNL Sun';};
             if ( exists( $starLinuxNodes{$host} ) ) {$starLinuxNodeCount++;$hostLoc{$host}='BNL Linux'};
             if ( exists( $starHpNodes{$host} ) ) {$starHpNodeCount++;$hostLoc{$host}='HP'};
+            if ( exists( $usatlasNodes{$host} ) ) {$usatlasNodeCount++;$hostLoc{$host}='USAtlas'};
             if ( exists( $starLblNodes{$host} ) ) {$starLblNodeCount++;$hostLoc{$host}='LBL'};
             if ( exists( $deskNodes{$host} ) ) {$deskNodeCount++;$hostLoc{$host}='Desktop'};
             if ( exists( $starRiceNodes{$host} ) ) {$starRiceNodeCount++;$hostLoc{$host}='Rice'};
@@ -150,6 +194,7 @@ foreach my $type ( sort keys %stattypes ) {
     printf(FSITES "BNL Sun   %6d\n",$starSunNodeCount);
     printf(FSITES "BNL Linux %6d\n",$starLinuxNodeCount);
     printf(FSITES "Desktop   %6d\n",$deskNodeCount);
+    printf(FSITES "US ATLAS  %6d\n",$usatlasNodeCount);
     printf(FSITES "HP        %6d\n",$starHpNodeCount);
     printf(FSITES "LBL       %6d\n",$starLblNodeCount);
     printf(FSITES "Rice      %6d\n",$starRiceNodeCount);
