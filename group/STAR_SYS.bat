a91fe085da852b251493772963e44d61a119b5a4
cvs2git <>
2000-08-25 20:21:43 +0000

    This commit was manufactured by cvs2svn to create tag 'SL00g'.

--- group/STAR_SYS.bat
+++
@@ -1,7 +0,0 @@
-rem # Determine STAR_SYS variable.
-rem
-set STAR_SYS=intel_wnt
-echo "STAR_SYS"="%STAR_SYS%">>Env.reg
-
-set STAR_HOST_SYS=%STAR_SYS%
-echo "STAR_HOST_SYS"="%STAR_HOST_SYS%">>Env.reg
