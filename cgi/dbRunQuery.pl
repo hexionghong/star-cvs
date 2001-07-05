2e012a5189be9bca12757e8b76241a683aded7da
jeromel <>
2001-07-05 19:05:12 +0000

    Added 'all' option

--- cgi/dbRunQuery.pl
+++ cgi/dbRunQuery.pl
@@ -36,61 +36,60 @@ my $datSet  =  $query->param('SetT');
 
 my $colSet;
 if ($collSet eq "AuAu130") {
- $colSet = "AuAu1";
-}else {
-$colSet = $collSet;
+    $colSet = "AuAu1";
+} else {
+    $colSet = $collSet;
 }
 
 my @joinSet = ( $colSet. "%" . $prodSr . "%" .$datSet ."%" . $detrSet);
 
 #####  connect to operation DB
 
- &StDbProdConnect();
+&StDbProdConnect();
 
 my $mmRun;
 my @runSet;
 my $nrunSet = 0;
 
-if($detrSet eq "all" and $datSet ne "all" ) {
 
- $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND trigger = '$datSet' AND dataset like '$colSet%' AND path like '%/200%' ";
+if ($detrSet eq "all" and $datSet ne "all" ) {
+    $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND trigger = '$datSet' AND dataset like '$colSet%' AND path like '%/200%' ";
 
-}elsif($detrSet ne "all" and $datSet ne "all" ) {
+} elsif ($detrSet ne "all" and $datSet ne "all" ) {
+    $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND trigger = '$datSet' AND dataset like '%$detrSet' AND dataset like '$colSet%' AND path like '%/200%' ";
 
- $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND trigger = '$datSet' AND dataset like '%$detrSet' AND dataset like '$colSet%' AND path like '%/200%' ";
-}elsif($detrSet ne "all" and $datSet eq "all" ) {
+} elsif ($detrSet ne "all" and $datSet eq "all" ) {
+    $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND dataset like '%$detrSet' AND dataset like '$colSet%' AND path like '%/200%' ";
 
- $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND dataset like '%$detrSet' AND dataset like '$colSet%' AND path like '%/200%' ";
-
-}elsif($detrSet eq "all" and $datSet eq "all" ) {
-
- $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND dataset like '$colSet%' AND path like '%/200%' ";
+} elsif ($detrSet eq "all" and $datSet eq "all" ) {
+    $sql="SELECT DISTINCT runID FROM $FileCatalogT WHERE jobID like '%$prodSr%' AND dataset like '$colSet%' AND path like '%/200%' ";
 }
 
-   $cursor =$dbh->prepare($sql)
-    || die "Cannot prepare statement: $DBI::errstr\n";
-   $cursor->execute;
- 
-    while(@fields = $cursor->fetchrow) {
-      my $cols=$cursor->{NUM_OF_FIELDS};
 
-        for($i=0;$i<$cols;$i++) {
-           my $fvalue=$fields[$i];
-           my $fname=$cursor->{NAME}->[$i];
+$cursor =$dbh->prepare($sql)
+    || die "Cannot prepare statement: $DBI::errstr\n";
+$cursor->execute;
+ 
+while(@fields = $cursor->fetchrow) {
+    my $cols=$cursor->{NUM_OF_FIELDS};
+
+    for($i=0;$i<$cols;$i++) {
+	my $fvalue=$fields[$i];
+	my $fname=$cursor->{NAME}->[$i];
 #        print "$fname = $fvalue\n" ;
        
-         $mmRun = $fvalue     if( $fname eq 'runID'); 
-         }
-        $runSet[$nrunSet] = $mmRun;
-        $nrunSet++;
- }
+	$mmRun = $fvalue     if( $fname eq 'runID'); 
+    }
+    $runSet[$nrunSet] = $mmRun;
+    $nrunSet++;
+}
 
 
 if($nrunSet == 0) {
-  $runSet[0] = "no data";
+    $runSet[0] = "no data";
 }
 
- &StDbProdDisconnect();      
+&StDbProdDisconnect();      
 
   
       
@@ -100,12 +99,12 @@ print $query->header;
 print $query->start_html('dbRunQuery');
 print $query->startform(-action=>"dbRunBrows.pl");  
 
-  print " <title>Query for Run Number</title>";
-  print "  </head>\n";
-  print "  <body bgcolor=\"#ffdc9f\"> \n";
-  print "  <h2 align=center>Run Numbers in Production $prodSr for $datSet Events </h2>\n";
-  print " </head>\n";
-  print " <body>";
+print " <title>Query for Run Number</title>";
+print "  </head>\n";
+print "  <body bgcolor=\"#ffdc9f\"> \n";
+print "  <h2 align=center>Run Numbers in Production $prodSr for $datSet Events </h2>\n";
+print " </head>\n";
+print " <body>";
 
 print <<END;
 </SELECT><br>
@@ -113,13 +112,14 @@ print <<END;
 <br>
 END
 
+ unshift(@runset,"all");
  print "<p>";
  print "<h2 align=center>Select Run Number:</h2>";
  print "<h4 align=center>";
  print $query->popup_menu(-name=>'runN',
-                    -values=>\@runSet,
-                      -size=>10
-                      );
+			  -values=>\@runSet,
+			  -size=>10
+			  );
 
 print <<END;
 </SELECT><br>
@@ -130,7 +130,7 @@ END
  print "<p>";
  print "<h4 align=center>";
  print $query->hidden(-name=>'prodSet',
-                    -values=>\@joinSet,
+		      -values=>\@joinSet,
                       );
 
 print <<END;
