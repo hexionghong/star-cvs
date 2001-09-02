1341c20008b84028e44c4cb216b6735751fc3feb
didenko <>
2001-09-02 02:08:36 +0000

    change to run in DEV

--- scripts/readMail.pl
+++ scripts/readMail.pl
@@ -41,7 +41,8 @@ if( defined($ARGV[0]) ){
     $prodl = $ARGV[0];
 } else {
     # default value
-    $prodl ="P01hg";                               # default production level
+#    $prodl ="P01hg";                               # default production level
+     $prodl ="DEV";
 }
 # ... so, also read the production tag from a conf file
 $HOME = $ENV{HOME};
