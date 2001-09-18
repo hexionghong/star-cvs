ec84368bed4f403925a053674e8b17411a89d73b
jeromel <>
2001-09-18 19:55:30 +0000

    Added support for multiple queues.

--- scripts/readMail.pl
+++ scripts/readMail.pl
@@ -15,6 +15,13 @@
 #  Configuration read from a file named readMail.conf if exists.
 #  Supported keywords :
 #       prodtag=XXXX       
+#       qnum=XXX
+#
+# By default, qnum=0 means ANY Email coming from the CRS system release
+# a new job to the queue. If this parameter is specified, only Emails
+# coming from that queue release a new job in that same queue for production
+# version prodtag.
+#
 ###############################################################################
 
 
@@ -28,6 +35,7 @@ my $job_line;
 my @wrd;
 my $date_line;
 my ($sec,$min,$hour,$mday,$mon);
+my $qnum=0;
 
 
 # Added J.Lauret June 11th 2001. Merged from the auto_submit.pl script.
@@ -40,12 +48,17 @@ my $prodl;
 if( defined($ARGV[0]) ){   
     $prodl = $ARGV[0];
 } else {
-    # default value
-    $prodl ="P01hg";                               # default production level
-#     $prodl ="DEV";
+    # default value for production level. DO NOT change this
+    # but rather use the starreco:./readMail.conf file to
+    # change the default.
+    $prodl ="P01hg";
 }
-# ... so, also read the production tag from a conf file
+
+
+# ... so, also read the production tag from a conf file. 
+# Warning : from a cronjob, $ENV{HOME} is null.
 $HOME = $ENV{HOME};
+if($HOME eq ""){ $HOME = ".";}
 if( -e "$HOME/readMail.conf"){
     if (open(FI,"$HOME/readMail.conf") ){
 	while( defined($line = <FI>) ){
@@ -54,6 +67,8 @@ if( -e "$HOME/readMail.conf"){
 		@items = split("=",$line);
 		if( $items[0] =~ m/prodtag/i){
 		    $prodl = $items[1];
+		} elsif ( $items[0] =~ m/qnum/i){
+		    $qnum  = $items[1];
 		}
 	    }
 	}
@@ -62,13 +77,12 @@ if( -e "$HOME/readMail.conf"){
 }
 
 
-
 # root for a structure for an implied directory structure made of a
 # 'jobfiles' directory and an 'archive' directory. This structure MUST
 # be under nfs-tree since there would be a token issue otherwise.
 my $SOURCE="/star/u/starreco/$prodl/requests/daq";
-
 my $SUBMIT="/usr/local/bin/crs_submit.pl";         # crs submit script
+my $PRIO=100;                                      # default job submit priority
 my $SFLAG=0;                                       # flag for job sub. Auto set
 my $SSUBM=1;                                       # set to 0 to disable submit
 
@@ -90,6 +104,7 @@ open(FO,">>mbox.piped");
 
 
 $outfile = "mail" . "_" .$thisday . "_" . "out"; 
+$QFLAG   = 1==1;
 
 while (<>) {
     chomp($mail_line = $_);
@@ -97,7 +112,12 @@ while (<>) {
 
     if ($mail_line =~ /Date/) {
 	$date_line = $mail_line;
-    } 
+    } elsif ($mail_line =~ m/(in queue )(\d+)( with priority)/){
+	# turn submission ON/OFF based on queue number
+	if( $qnum != $2 && $qnum != 0){
+	    $QFLAG = 1==0;
+	}
+    }
 
     if ($mail_line =~ /job_\d+/) {
 	$SFLAG += 1; # must be at least 2
@@ -148,11 +168,11 @@ close(FO);
 if( defined($date_line) ){
     open (OUT,">> $outfile") or die "Can't open $outfile";
     print OUT $date_line, "\n";
-    print OUT "JobInfo:   %  $jbStat  %  $nodeID  %  $job_file\n"; 
+    print OUT "JobInfo:   %  $jbStat  %  $nodeID  %  $job_file % $qnum \n"; 
     close (OUT);
 }
 
-if ($SFLAG == 2 && $SSUBM){
+if ($SFLAG == 2 && $QFLAG && $SSUBM){
     # Now, the logic for file submission. Simple and fast ...
     opendir(JDIR,"$SOURCE/jobfiles/");
 
@@ -161,7 +181,11 @@ if ($SFLAG == 2 && $SSUBM){
 	    $lock = "$SOURCE/jobfiles/$file.lock";
 	    if( ! -e "$lock" ){
 		if ( open(FO,">$lock") ){
-		    system("$SUBMIT $SOURCE/jobfiles/$file");
+		    if( $qnum !=0){
+		      system("$SUBMIT $SOURCE/jobfiles/$file $PRIO $qnum");
+		    } else {
+		      system("$SUBMIT $SOURCE/jobfiles/$file $PRIO");
+		    }
 		    rename("$SOURCE/jobfiles/$file","$SOURCE/archive/$file");
 		    # Just for the heck of it, output submit debugging
 		    &ASLog("Job $SOURCE/jobfiles/$file submitted");
