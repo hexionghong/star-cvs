f82ea478a24a4ebfdd3909d2d3cedcfdae64bb4b
jeromel <>
2001-11-21 20:54:53 +0000

    This .pl used as a pm MUST be terminated by a TRUE return value. Otherwise, require/use statements will fail.

--- group/STAR_SYS.pl
+++ group/STAR_SYS.pl
@@ -5,8 +5,10 @@
 # T. Wenaus
 #
 $STAR_SYS='';
+
 if (-e '/usr/afsws/bin/sys') {$STAR_SYS=`/usr/afsws/bin/sys`}
 $STAR_HOST_SYS=$STAR_SYS;
 
 # be lazy ... leave the tedious AFS-isn't-there part for some other time!!!
 # Anyway, nowadays even on Linux we have real AFS.
+1;
