b726eb03bbd46ce2443ec09202129f0df83cb103
jeromel <>
2002-03-17 20:36:16 +0000

    Several modifications as follow :  - Support for perl patterns (but must contain at least one *)  - Fix for modified CRS drop queue mechanism (actually marked as the original    queue which screws up the count per queue. Rely on node instead)  - Non repeated warning (using logical flags)  - Drastic change of logic for in getcnt(). Now sorts out all slots available    per queue and supports drop>1 (unlike the CRS submit script). This makes    possible constent filling of the entire farm.  - Added mode drop < 0 which make N-1 jobs in each queue. Default (drop > 0)    is N jobs=MAX in the upper queue and N-1 in each lower queues.

--- scripts/CRSQueues.pm
+++ scripts/CRSQueues.pm
@@ -6,7 +6,7 @@
 
 use Carp;
 
-package CRSQueues;
+package CRSQueues2;
 require 5.000;
 require Exporter;
 @ISA = qw(Exporter);
@@ -22,6 +22,19 @@ $JOBINF="/usr/local/bin/crs_status.pl -c";
 $SUBMIT="/usr/local/bin/crs_submit.pl";
 #$SUBMIT="/usr/crs/bin/CRS_submit_awc.pl";
 
+
+$CRSQ::MAXQ=5;                  # support up to that number of queues 
+$CRSQ::PFACT=5;                 # some number of files arbitrary proportion factor
+$CRSQ::OFFSYNC=20;              # queue submission will continue up to this %tage
+                                # of off-sync.
+$CRSQ::GETCNT=0;                # called the getcnt() routine (not a config var)
+
+
+# %CRSQ::TOT     assoc-array of queue info (total slots)
+# %CRSQ::RUN     assoc-array of running jobs pass1
+# %CRSQ::FND     assoc-array of found jobs pass2
+# %CRSQ::DIF     hold initial differences (available slots)
+# %CRSQ::WARN    Avoid multiple warnings by flagging it
 
 #
 # This routine checks the job files matching $pat
@@ -55,7 +68,7 @@ sub CRSQ_check
     }
 
     # Scan the current directory for all job files
-    @all = glob($pat);
+    @all = &Glob($pat);
     
     foreach $jfile (@all){
 	if( ! defined($JOBS{$jfile}) ){
@@ -76,12 +89,19 @@ sub CRSQ_check
 }
 
 
+
+
 # Check the number of slots in queue $qnum. 
-# If $drop is specified, check all queues lt than $qnum.
-# If $pat is specified, check files with that wildcard pattern. If plenty
+#
+#  If $drop is specified, check all queues lt than $qnum but no less than
+# $qnum - $drop. Not this routine calculates for MAX jobs for queue $qnum 
+# and MAX-1 for lower queues. However, if $drop is negative, $MAX-1 will
+# enter in the base calculation of the number of available slots for $qnum.
+#
+#  If $pat is specified, check files with that wildcard pattern. If plenty
 # exists, return an error. See comments for this obscure mode ...
-# This can be disabled by using $nchk parameter (in case the job files
-# are pre-generated, we do not want to do this check).
+#  The input file parsing can be disabled by using $nchk parameter (in case the 
+# job files are pre-generated, we do not want to do this check).
 #
 # Returns the total number of available slots.
 #
@@ -89,50 +109,74 @@ sub CRSQ_getcnt
 {
     my($qnum,$drop,$pat,$nchk)=@_;
 
-    my($line);
+    my($i,$line,$ratio);
     my(@result,@items);
-    my($ATOT,$TOT,$TOTS,$NOTA,$SAVT);
+    my($NOTA,$TOT,$DEBUG);
     my(%NODES);
-    
-    # Dummy initialization
-    $NOTA = $TOTS = $TOT = 0;
 
-    # get queue status first
+
+    $DEBUG = 0;
+    
+    # Sanity check
+    if($qnum >= $CRSQ::MAXQ){  
+	return &Exceed("QueueNum",$CRSQ::MAXQ);
+    }
+
+
+    # Initialization
+    $CRSQ::GETCNT = 1;
+    $NOTA = 0;
+    for($i=0 ; $i < $CRSQ::MAXQ ; $i++){   
+	$CRSQ::FND{$i} = $CRSQ::RUN{$i} = $CRSQ::TOT{$i} = 0;
+	if( ! defined($CRSQ::WARN{$i}) ){ $CRSQ::WARN{$i} = 1;}
+    }
+
+
+    # Get queue status and counts first -- Pass 1
     @result = `$STATUS`;
     foreach $line (@result){
 	chomp($line);
 	@items = split("%",$line);
 
 	if( $items[1] ne "unavailable"){
-	    if ($drop){
-		# In drop mode, all queues number < $qnum are available to us
-		# However, we will put only 2 jobs in the 'other'
-		# queues and 3 in the $qnum.
-		if ($items[4] == $qnum){
-		    $TOT  += $items[2];          # requested queue total
-		    $NODES{$items[0]} = 1;
-		} elsif ($items[4] >= ($qnum-$drop) ) {
-		    $TOTS += $items[2]-1;        # other queue total
-		}
-	    } else {
-		if ($items[4] == $qnum){
-		    # choice of -1 (just the exact number) or
-		    # not (add one more in the queue).
-		    $TOT += $items[2]-1;         # requested queue total
-		    $NODES{$items[0]} = 1;
-		}
-	    }
+	    $i = $items[4];
+	    $CRSQ::TOT{$i}       += $items[2];
+	    $CRSQ::RUN{$i}       += $items[3];
+	    $NODES{$items[0]}    += $i;
+
+	    if( ($i < $qnum && $drop && $i != 0) || ($drop < 0)) {
+		# Reduce by one comparing to MAX.
+		$CRSQ::TOT{$i}--;
+	    } 
+
 	} else {
+	    # Number of un-available nodes for any reasons
 	    $NOTA++;
 	}
     }
+    $drop = abs($drop);
+
+
+    # Calculate the total number of available jobs 
+    foreach $i (keys %CRSQ::TOT){
+	if( $i == 0){ next;}
+	#printf 
+	#    "Queue=%d Tot=%3d Run=%3d Dif=%3d\n",
+	#    $i,$CRSQ::TOT{$i},$CRSQ::RUN{$i},
+	#    ($CRSQ::TOT{$i}-$CRSQ::RUN{$i});
+
+	$CRSQ::DIF{$i} = $CRSQ::TOT{$i}-$CRSQ::RUN{$i};
+	$TOT += $CRSQ::TOT{$i};
+    }
+
+
+    # Extra sanity check. If the first command did not return
+    # any information, no need to continue ...
     if($TOT == 0){
-	print "CRSQ :: Error on ".localtime().", none of the queues are available\n";
+	print "CRSQ :: Error on ".localtime().
+	    ", none of the queues are available\n";
 	return -1;
     }
-    #print "$TOT $TOTS $NOTA\n";
-    $SAVT = $TOT;
-    $ATOT = $TOT+$TOTS;
 
 
     # This small test implies that the job files will be moved
@@ -141,24 +185,20 @@ sub CRSQ_getcnt
     # queues goes beserk and the crs_status does not return 
     # any information. This leads to a bad count of jobs and
     # available slots (0 actually) and subsequently, too many 
-    # submission ... The pattern find will prevent this. 
+    # submission ... The pattern find would prevent this. 
     if( defined($pat) && ! defined($nchk) ){
-	@all = glob($pat);
-	if($#all > 10*$ATOT ){
+	@all = &Glob($pat);
+	if($#all > $CRSQ::PFACT*$TOT ){
 	    print 
 		"CRSQ :: Warning on ".localtime().", there are ",
 		"$#all job files found as $pat\n";
-	    if($ATOT == 0){
-		print 
-		    "        There are 0 jobs running. Check for stale/dead jobs\n";
-	    } else {
-		# Stop, this condition is ab-normal
-		return -1;
-	    }
+	    # Stop, this condition is ab-normal
+	    return -1;
 	} 
     }
 
 
+    # --- Pass 2
     # Get the number of jobs actually on their way
     # to those queues. 'staging' or 'staged' jobs would not 
     # necessarily show up in the -m list. 
@@ -171,47 +211,113 @@ sub CRSQ_getcnt
 	chomp($line);
 	@items = split("%",$line);
 
-	if ($drop){
-	    if($items[4] == $qnum ){
-		if( defined($NODES{$items[3]}) ||
-		    $items[2] =~ /stag/){          # staged or staging
-		    $TOT  = $TOT  - 1;
-		}
-		# Otherwise, those are dying/finishing spill-over
-		# jobs. Staging and Staged jobs needs to be counted
-		# because we do not not know where they belong.
-	    } elsif ( $items[4] >= ($qnum-$drop) ) {
-		$TOTS = $TOTS - 1;
-	    }
+	# complete the list. Easier for later testing.
+	if( ! defined($NODES{$items[3]}) ){ $NODES{$items[3]} = 0;}
+
+	$i = $NODES{$items[3]};
+	if( $i != 0){
+	    # This node is knows, hold queue $i and is busy
+	    $CRSQ::FND{$i}++;
 	} else {
-	    if($items[4] == $qnum){
-		$TOT = $TOT - 1;
-	    }
+	    # the best we can do is to count on the queue info
+	    $CRSQ::FND{$items[4]}++;
 	}
     }
-    #print "$TOT\n";
 
-    # Check TOT only in non-spill mode. In spill mode, the new
-    # count TOT+TOTS will give the accurate number of slots.
-    if($TOT < 0 && ! $drop && $TOTS != 0){ 
-	$TOT = -$TOT;
-	print 
-	    "CRSQ :: Error: We have $TOT/$TOTS more jobs than expected ",
-	    "(Max jobs should be $ATOT, $SAVT found, unavailable $NOTA) on ",
-	    localtime()."\n";
-	return -1;
+
+    # Recalculate the total number of real slots we may use
+    $TOT = 0;
+    foreach $i (keys %CRSQ::TOT){
+	if( $i == 0){ next;}
+	if( ($i == $qnum && ! $drop)                         ||
+	    ($i <= $qnum && $i >= ($qnum - $drop) && $drop)) {
+
+	    if($DEBUG){
+		printf 
+		    ">> Queue=%d Tot=%3d Run=%3d Found=%3d Diff=%3d\n",
+		    $i,$CRSQ::TOT{$i},$CRSQ::RUN{$i},$CRSQ::FND{$i},$CRSQ::DIF{$i};
+	    }
+
+	    # check if something else has submitted jobs there
+	    if ( $CRSQ::DIF{$i} < 0 ){
+		if( $CRSQ::WARN{$i} ){
+		    $CRSQ::DIF{$i} = - $CRSQ::DIF{$i};		    
+		    print 
+			"CRSQ :: There are $CRSQ::DIF{$i} more jobs in queue $i ",
+			"than what we intended to use on ".localtime()."\n";
+		    $CRSQ::WARN{$i} = 0;
+		}
+		$CRSQ::DIF{$i}  = 0;
+
+	    } elsif( $CRSQ::FND{$i}  > $CRSQ::RUN{$i}){
+		# After the counting, we have found FND jobs but
+		# RUN ones in pass 1 . Those are the un-accounted for
+		# jobs in staged/staging or other weird states.
+
+		# Make a diff adjustment
+		if ( $CRSQ::DIF{$i} != 0 ){
+		    $ratio = ($CRSQ::FND{$i}-$CRSQ::RUN{$i})/$CRSQ::DIF{$i}*100 ;
+		} else {
+		    $ratio = 100;
+		}
+
+		# The OFFSYNC value will decide on whether or not we
+		# continue to submit jobs in this case.
+		if( $ratio < $CRSQ::OFFSYNC ){
+		    if( $CRSQ::WARN{$i} ){
+			print 
+			    "CRSQ :: Queue $i not in sync but within margin ",
+			    "(Found $CRSQ::FND{$i} vs $CRSQ::RUN{$i} running over ",
+			    " $CRSQ::DIF{$i} slots). Adjusting on ".localtime()."\n";
+			$CRSQ::WARN{$i} = 0;
+		    }
+		    $CRSQ::DIF{$i} -= ($CRSQ::FND{$i}-$CRSQ::RUN{$i});
+		    if( $CRSQ::DIF{$i} < 0){ $CRSQ::DIF{$i} = 0;}
+		} else {
+		    if( $CRSQ::WARN{$i} ){
+			print 
+			    "CRSQ :: Queue $i not in sync. Bootstrap ",
+			    "found $CRSQ::FND{$i} but $CRSQ::RUN{$i} running ",
+			    "on ".localtime()."\n";
+			$CRSQ::WARN{$i} = 0;
+		    }
+		    $CRSQ::DIF{$i}  = 0;
+		}
+
+	    } else {
+		# All OK (there are available slots) or nothing to do.
+		$CRSQ::WARN{$i} = 1;
+
+	    }
+
+
+	    $TOT += $CRSQ::DIF{$i};
+
+	} else {
+	    # This needs to be reset for later use (submit takes 
+	    # advantage of this).
+	    $CRSQ::DIF{$i} = 0;
+	}
     }
-    undef($ATOT);
+
+    # mem cleanup
     undef(@items);
     undef(@result);
 
+    if( $TOT != 0){
+	print "CRSQ :: ";
+	foreach $i (keys %CRSQ::TOT){
+	    print "Queue=$i ($CRSQ::DIF{$i}) " if ($CRSQ::TOT{$i} != 0);
+	}
+	print "\n";
+    }
+
+    #die "$TOT\n";
 
     # We don't care of what is happening to the spill-over
     # queue pool. If the count is incorrect there, just set
     # to 0.
-    if($TOT  < 0 && $TOTS <= 0){  return 0;}
-    if($TOTS < 0){                return $TOT;}
-    return $TOT+$TOTS;
+    return $TOT;
 }
 
 
@@ -220,10 +326,18 @@ sub CRSQ_getcnt
 # Submit $jfile with priority $prio. Eventually use a queue
 # shift of $drop.
 #
+# Note that the last 2 arguments are not mandatory if
+# the getcnt() routine was called. If you chose to submit
+# that way, no jobs will be submitted after the getcnt()
+# stack gets exhausted. If you still want to persist in
+# submitting something, you must at least specify a default
+# $qnum value. This latest mode is not encouraged ...
+#
+#
 sub CRSQ_submit
 {
     my($jfile,$prio,$qnum,$drop)=@_;
-    my($res);
+    my($res,$i,$q);
 
     if ( ! -e $jfile){
 	print 
@@ -231,11 +345,38 @@ sub CRSQ_submit
 	    "Cannot submit to queue $qnum\n";
 	0;
     }
+    if( ! defined($qnum) ){  $qnum = 0;}
+    if( ! defined($drop) ){  $drop = 0;}
+    $drop = abs($drop);
+
+    if( $CRSQ::GETCNT ){
+	#print "GETCNT() was called\n";
+	foreach $i (sort {$b <=> $a} keys %CRSQ::DIF){
+	    $q = $i;
+	    if( $CRSQ::DIF{$i} != 0){ 
+		$CRSQ::DIF{$i}--;
+		last;
+	    }
+	}
+	if( $q == 0 ){
+	    # exhausted. Cannot make it that way
+	    $CRSQ::GETCNT = 0;
+	    # We will therefore comply with the exact command
+	    # i.e. submitting in queue $qnum with $drop as
+	    # arguments says. However, if we used that function
+	    # with qnum=0, we have to leave now.
+	    if( $qnum == 0){ return 0;}
+	} else {
+	    $drop = 0;
+	    $qnum = $q;
+	    #print "We have selected queue $q $CRSQ::DIF{$q}\n";
+	}
+    } 
 
     $res = `$SUBMIT $jfile $prio $qnum $drop`;
     $res =~ s/\n//g;
     if( $res =~ m/queue $qnum with priority $prio/){ 
-	1;
+	$qnum;
     } else {
 	if ( $res =~ m/(queue\s+)(\d+)/ ){
 	    print "CRSQ :: Failed to submit $jfile $qnum -> $2 => [$res]\n";
@@ -247,4 +388,35 @@ sub CRSQ_submit
 }
 
 
+# 
+# Stupid blabla routine
+#
+sub Exceed
+{
+    my($param,$max)=@_;
+
+    print "CRSQ :: Received a parameter exceeding expected range. $param < $max\n";
+    return 0;
+}
+
+sub Glob
+{
+    my($pat)=@_;
+    my(@all);
+
+    # support perl patterns
+    if( $pat =~ /\.\*/){
+	# a .* like . 
+	opendir(DIR,".");
+	@all = grep { /$pat/ } readdir(DIR);
+	close(DIR);
+    } else {
+	# use glob although this may fail
+	@all = glob($pat);
+    }
+    @all;
+}
+
+
 1;
+
