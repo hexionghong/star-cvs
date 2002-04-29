a37fa7781553fc76477afd07afb92cf4b46b3ec7
didenko <>
2002-04-29 16:59:24 +0000

    fixed .event.root

--- cgi/dbSumRun.pl
+++ cgi/dbSumRun.pl
@@ -78,7 +78,7 @@ my  @hpssInFiles;
  my @hpssDstFiles;
   $nhpssDstFiles = 0;
 
-  $sql="SELECT runID, dataset, fName, path, size, Nevents, trigger  FROM $FileCatalogT WHERE runID = '$runNum' AND fName LIKE '%event.root' AND path like '%$prodSr%' AND hpss = 'Y'";
+  $sql="SELECT runID, dataset, fName, path, size, Nevents, trigger  FROM $FileCatalogT WHERE runID = '$runNum' AND fName LIKE '%.event.root' AND path like '%$prodSr%' AND hpss = 'Y'";
   $cursor =$dbh->prepare($sql)
     || die "Cannot prepare statement: $DBI::errstr\n";
   $cursor->execute;
@@ -214,7 +214,7 @@ print <<END;
           <title>Production Summary for Real Data</title>
    </head>
    <body BGCOLOR=\"#ccffff\"> 
-<h5><a href=\"http://www.star.bnl.gov/STAR/comp/prod/ProdSummary.html\">Production</a><br>
+<h5><a href=\"http://www.star.bnl.gov/STARAFS/comp/prod/ProdSummary.html\">Production</a><br>
 <a href=\"http://www.star.bnl.gov/devcgi/dbProdDAQQuery.pl\">Production Series Query </a><br>
      <h2 align=center>Production Summary for $prodSr <br> and Run Number $runNum</h2>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 >
