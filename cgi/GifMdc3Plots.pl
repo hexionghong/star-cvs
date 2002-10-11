78e9ea9f18ff557ad476e62687e3e1810a9aa6b2
didenko <>
2002-10-11 15:02:20 +0000

    *** empty log message ***

--- cgi/GifMdc3Plots.pl
+++ cgi/GifMdc3Plots.pl
@@ -8,7 +8,7 @@
 # Script to create plots for production operation
 #================================================================== 
 
-require "/afs/rhic/star/packages/DEV00/mgr/dbCpProdSetup.pl";
+require "/afs/rhic/star/packages/scripts/dbCpProdSetup.pl";
 
 use CGI;
 use GIFgraph::linespoints;
