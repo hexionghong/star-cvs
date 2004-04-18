928d577eb1fdf5e7934a4c44c6bb266e9b72e695
jeromel <>
2004-04-18 23:25:09 +0000

    Revived Nikita's script. Extended finctionalities.

--- scripts/MoveJob.pl
+++ scripts/MoveJob.pl
@@ -4,94 +4,106 @@
 #use strict;
 use File::Copy;
 
-#my $workdir = "/star/u/nikita/daq/";#testdir
-my $workdir = "/afs/rhic/star/doc/www/html/tmp/csh/";
+my $workdir = "/afs/rhic.bnl.gov/star/doc/www/html/tmp/csh/";
 
 my $target;
 my $source;
 my $workfile;
 my $ctime;     #list creation time
-my $i;       #file num
+my $i;         #file num
 my $j;         #move str num
 my %mvstr;     #move strings
 my %mvfiles;   #move file names
 my @wf;        #work files
+my $extended=1;
+
+my($SELF)="MoveJob";
 
 &scanfiles();
 
 #&print_menu();
-print
-    "\nType '?' for help.\n\n";
-print
-    "MoveJob> ";
-$actn = <STDIN>;
-chomp($actn);
+print 
+    "\nType '?' for help.\n\n",
+    "$SELF> ";
+chomp($actn = <STDIN>);
+
 while( $actn ne "q"){  
     $actn =~ s/\s+/ /g; 
-    @nums = split(" ",$actn);
-    if( defined($nums[0]) ){
-	if( ($nums[0] eq "l") or ($nums[0] eq "list")){
-	    #show the list of files with contents
-	    &scanfiles();
-	}
-	if( (($nums[0] eq "d") or ($nums[0] eq "del")) and (defined($fnum = $nums[1])) ){
-	    #delete command
-	    if( $#nums >= 2 ){
-		$i = 2;
-		while( defined($nums[$i]) ){
-		    &del_str("$fnum"." "."$nums[$i++]");
-		}
-	    }elsif( defined($mvfiles{$fnum}) ){
-                #delete entire file cmnd
-		&question("delete file : $mvfiles{$fnum} ? [yes] : ");
-		if( ($actn eq "yes\n") or ($actn eq "\n") ){
-		    print
-			"deleted $mvfiles{$fnum}\n";
-		    unlink("$mvfiles{$fnum}");
-		}
-	    }    
-	}
-	if( (($nums[0] eq "m") or ($nums[0] eq "move")) and defined($fnum = $nums[1]) ){
-	    #move command
-	    if( $#nums >= 2 ){
-                #move selected cmnd
-		$i = 2;
-		while( defined($nums[$i]) ){
-		    &move_str("$fnum"." "."$nums[$i++]","one");
-		}
-	    } elsif( defined($mvfiles{$fnum}) ){
-		# move entire file cmnd
-		&question("move $mvfiles{$fnum} ? [yes] : ");
-		if( ($actn eq "yes\n") or ($actn eq "\n") ){
-		    foreach $key (keys(%mvstr)){
-			@nums = split(" ",$key);
-			if( $nums[0] == $fnum ){
-			    &move_str("$fnum"." "."$nums[1]","all");
+    # we will allow commands separated by ";"
+    @items = split(";",$actn);
+
+    foreach $actn (@items){
+	@nums = split(" ",$actn);
+
+	if( defined($nums[0]) ){
+	    if( ($nums[0] eq "l") or ($nums[0] eq "list")){
+		#show the list of files with contents
+		&scanfiles();
+
+	    } elsif( (($nums[0] eq "d") or ($nums[0] eq "del")) and (defined($fnum = $nums[1])) ){
+		#delete command
+		if( $#nums >= 2 ){
+		    $i = 2;
+		    while( defined($nums[$i]) ){
+			&del_str("$fnum"." "."$nums[$i++]");
+		    }
+		} elsif( defined($mvfiles{$fnum}) ){
+		    #delete entire file cmnd
+		    &question("delete file : $mvfiles{$fnum} ? [yes] : ");
+		    if( ($actn eq "yes\n") or ($actn eq "\n") ){
+			if ( unlink("$mvfiles{$fnum}") ){
+			    print "deleted $mvfiles{$fnum}\n";
+			} else {
+			    print "Failed to delete $mvfiles{$fnum}\n";
 			}
 		    }
 		}
-	    }    
-	}
-	if( ($nums[0] eq "?") or ($nums[0] eq "help") or ($nums[0] eq "h") ){
-	    # show help
-	    &print_menu();
-	}
-	if( ($nums[0] eq "ex") or ($nums[0] eq "e") ){
-	    # show example
-	    print
-		"\nUsing commands example:\n\n",
-		"MoveJob> move 2           move ALL jobs from file 2\n",
-		"MoveJob> move 2 2         move from file 2 ONLY job 2\n",
-		"MoveJob> move 2 1 2 3     move from file 2 jobs 1, 2 AND 3\n",
-		"MoveJob> del 2            delete(unlink) file 2\n",
-		"MoveJob> del 2 2          delete from file 2 ONLY job 2\n",
-		"MoveJob> del 2 1 2 3      delete from file 2 jobs 1, 2 AND 3\n",
+    
+	    } elsif( (($nums[0] eq "m") or ($nums[0] eq "move")) and defined($fnum = $nums[1]) ){
+		#move command
+		if( $#nums >= 2 ){
+		    #move selected cmnd
+		    $i = 2;
+		    while( defined($nums[$i]) ){
+			&move_str("$fnum"." "."$nums[$i++]","one");
+		    }
+		} elsif( defined($mvfiles{$fnum}) ){
+		    # move entire file cmnd
+		    &question("move $mvfiles{$fnum} ? [yes] : ");
+		    if( ($actn eq "yes\n") or ($actn eq "\n") ){
+			foreach $key (keys(%mvstr)){
+			    @nums = split(" ",$key);
+			    if( $nums[0] == $fnum ){
+				&move_str("$fnum"." "."$nums[1]","all");
+			    }
+			}
+		    }
+		}    
+
+	    } elsif( ($nums[0] eq "?") or ($nums[0] eq "help") or ($nums[0] eq "h") ){
+		# show help
+		&print_menu();
+
+	    } elsif( ($nums[0] eq "ex") or ($nums[0] eq "e") ){
+		# show example
+		print
+		    "\nUsing commands example:\n\n",
+		    "$SELF> move 2           move ALL jobs from file 2\n",
+		    "$SELF> move 2 2         move from file 2 ONLY job 2\n",
+		    "$SELF> move 2 1 2 3     move from file 2 jobs 1, 2 AND 3\n",
+		    "$SELF> del 2            delete(unlink) file 2\n",
+		    "$SELF> del 2 2          delete from file 2 ONLY job 2\n",
+		    "$SELF> del 2 1 2 3      delete from file 2 jobs 1, 2 AND 3\n";
+
+	    } elsif( ($nums[0] eq "xtend") or ($nums[0] eq "x") ){
+		$extended = ! $extended;
+		print "eXtended display is now ".($extended?"ON":"OFF")."\n";
+	    }
+	    
 	}
     }
-    print
-	"\nMoveJob> ";
-    $actn = <STDIN>;
-    chomp($actn);
+    print "\n$SELF> ";
+    chomp($actn = <STDIN>);
 }
 
 #subs
@@ -102,7 +114,7 @@ sub question {
     do {
 	print "$q";
 	$actn = <STDIN>;
-    } while( ($actn ne "no\n") and ($actn ne "\n") and ($actn ne "yes\n") );
+    } while( ($actn !~ /no/i ) and ($actn ne "\n") and ($actn !~ /yes/i) );
 
 }     
     
@@ -142,10 +154,9 @@ sub move_str{
 	}	
 	if( ($status eq "all") or ($actn eq "\n") or ($actn eq "yes\n") ){
 	    if( copy($str[0], $str[1]) ){
-		print 
-		    "copied $str[0] to $str[1]\n";
-	    }else{
-		warn"cannot copy file : $!\n";
+		print "copied $str[0] to $str[1]\n";
+	    } else {
+		warn  "cannot copy file : $!\n";
 	    }
 	    
 	}
@@ -154,7 +165,7 @@ sub move_str{
 #========================================
 sub print_menu{
     print 
-	"\nMoveJob commands:\n",
+	"\n$SELF commands:\n",
 	"?         (h)       Display this text\n",
 	"help      (h)       Synonim for '?'\n",
 	"list      (l)       Show list of files\n",
@@ -162,8 +173,10 @@ sub print_menu{
 	"move X Y  (m X Y)   Move job Y from file X\n",
 	"del X     (d X)     Delete file X\n",
 	"del X Y   (d X Y)   Delete Y job from file X\n",    
+	"xtend     (x)       eXtended display (list files to be moved)\n",
 	"ex        (e)       Example\n",
-	"quit      (q)       Quit MoveJob\n\n";
+	"command1 ; command2 ; command3 ... for multiple commands\n",
+	"quit      (q)       Quit $SELF\n\n";
 }
 #========================================
 sub scanfiles{
@@ -173,6 +186,8 @@ sub scanfiles{
     undef(@wf);
     undef(%mvstr);
     undef(%mvfiles);
+    my ($arch_dir,$match);
+
     @wf = glob("$workdir*.ml");
     foreach $workfile ( @wf ){  
 	if( open(FI,$workfile) ){ 
@@ -180,7 +195,7 @@ sub scanfiles{
 	    $workfile =~ /.*\/(\d*)\.ml/;
 	    $ctime = localtime($1);
 	    print 
-		$i." : $workfile      $ctime\n\n";
+		$i." : $workfile      $ctime\n";
 	    $j=0;       
 	    while( defined($str=<FI>) ){   
 		undef(@str);     
@@ -189,22 +204,30 @@ sub scanfiles{
 		if( $str[0] =~ /(.*archive\/)(.*st_physics.*)/ ){
 		    $arch_dir = $1;
 		    $match = $2;
+		} elsif( $str[0] =~ /(.*archive\/)(.*st_zerobias.*)/ ){
+		    $arch_dir = $1;
+		    $match = $2;
 		}
-		if( -e $target ){
-		    if( $source = `ls $arch_dir$match` ){
-			chomp($source);
-			undef($movestr);
-			$movestr .= $source."   ".$target; 
-			$mvstr{"$i"." "."$j"} = $movestr;
-			print
-			    "$i"." $j"." $movestr\n";
-			$j++;
+
+		if( -e $target && defined($match) ){
+		    @all = glob($arch_dir.$match);
+		    if ($#all != -1){
+			$source = $all[0];
+			if( -e $source ){
+			    chomp($source);
+			    $movestr = $source."   ".$target; 
+			    $mvstr{"$i"." "."$j"} = $movestr;
+			    if($extended){
+				print "\t$i"." $j"." $movestr\n";
+			    }
+			    $j++;
+			}
 		    }
+		} else {
+		    if($extended){ print "\t$target not found or pattern is not st_physics for $str[0]\n";}
 		}	
 	    }
-#	    print
-#		"\n";
-	}else{
+	} else {
 	    warn "cannot open $workfile : $!\n";
 	}
 	$i++;
