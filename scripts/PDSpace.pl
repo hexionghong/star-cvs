7b3af569e88845d1ec732dd24705cd05eb0dad6f
jeromel <>
2004-09-06 16:24:47 +0000

    Added production location table

--- scripts/PDSpace.pl
+++ scripts/PDSpace.pl
@@ -23,6 +23,9 @@ $BREAK{"03"} =  "Reserved Usage Space Area";
 $BREAK{"06"} =  "Production Disks / Assigned TEMPORARY space for Projects";  
 $BREAK{"08"} =  "Production Disks";  
 
+# Standard header style
+$TD  = "<TD BGCOLOR=\"black\" align=\"center\"><FONT FACE=\"Arial, Helvetica\"><FONT COLOR=\"white\"><B>";
+$ETD = "</FONT></B></FONT></TD>";
 
 
 for ( $i = $MIN ; $i <= $MAX ; $i++){
@@ -63,6 +66,8 @@ foreach $disk (@DISKS){
     undef(@TRGS);
     undef(%LIBS);
 
+    # Logic sorting our the trigger level and the library
+    # level. This implies a strict naming convention.
     if( -d "$disk/reco"){
 	@TMP = glob("$disk/reco*/*");
 	foreach $trg (@TMP){
@@ -77,6 +82,11 @@ foreach $disk (@DISKS){
 		#print "\tFound $ver\n";
 		$ver =~ s/.*\///;
 		$LIBS{$ver} = 1;
+		if ( defined($ALIBS{$ver}) ){
+		    $ALIBS{$ver} .= "$tmp;$disk ";
+		} else {
+		    $ALIBS{$ver}  = "$tmp;$disk ";
+		}
 	    }
 	}
 
@@ -96,7 +106,7 @@ foreach $disk (@DISKS){
 
     $ver = " ";
     foreach $tmp (keys %LIBS){
-	$ver .= "$tmp ";
+	$ver .= "<A HREF=\"#".&GetRef($tmp)."\">$tmp</A> ";
     }
     $DINFO{$disk} .= "$ver;";
     #print STDERR "$disk --> $DINFO{$disk}\n";
@@ -115,13 +125,20 @@ print $FO
     "<BODY>\n",
     "<H1 ALIGN=\"center\">Disk space overview</H1>\n",
     "<h5 align=\"center\">Generated on ".localtime()."</h5>\n",
-    "<h2>Disk space overview</h2>\n",
+    "<H1>Information</H1>\n",
+    "<OL>\n",
+    "<LI><A HREF=\"#DSO\">Disk Space Overview</A>",
+    "<LI><A HREF=\"#PLOC\">Production locations</A>",
+    "<LI><A HREF=\"#SUM\">Summary</A>",
+    "</OL>\n",
+    "\n",
+    "<H2><A NAME=\"DSO\">Disk space overview</A></H2>\n",
     "Note that each disk may contain other directories than reco*/. Those ",
     "are the only ones scanned ...\n",
     "The reported structure reflects a tree assumed to be of the form ",
     " Trigger/Field/Production.\n",
     "<P>\n",
-    "<table border=\"0\">\n<TR><td>Color Scale</TD>\n";
+    "<TABLE border=\"0\">\n<TR><TD>Color Scale</TD>\n";
 
 for ($i=0 ; $i <= $#COLORS ; $i++){
     $low  = int(100*$i/($#COLORS+1));
@@ -132,10 +149,8 @@ for ($i=0 ; $i <= $#COLORS ; $i++){
 print $FO 
     "</TR>\n</TABLE>\n",
     "<P>\n",
-    "<table border=\"1\" cellspacing=\"0\" width=\"1000\">\n";
+    "<TABLE border=\"1\" cellspacing=\"0\" width=\"50\">\n";
 
-$TD  = "<TD BGCOLOR=\"black\" align=\"center\"><FONT FACE=\"Arial, Helvetica\"><FONT COLOR=\"white\"><B>";
-$ETD = "</FONT></B></FONT></TD>";
 
 printf $FO
     "<TR>$TD%10s$ETD $TD%11s$ETD $TD%11s$ETD $TD%11s$ETD $TD%3s$ETD $TD%s$ETD $TD%s$ETD</TR>\n",
@@ -145,8 +160,6 @@ printf $FO
 $col     = 0;
 @$totals = (0,0,0);
 foreach $disk (sort keys %DINFO){
-    $tdisk =  $disk;
-    $tdisk =~ s/\//_/g;    # used for name reference
 
     #print STDERR "$DINFO{$disk}\n";
     @items = split(";",$DINFO{$disk});
@@ -172,7 +185,7 @@ foreach $disk (sort keys %DINFO){
 	    "<FONT COLOR=\"white\"><B>$BREAK{$1}</B></FONT></TD></TR>\n";
     }
     printf $FO
-	"<TR height=\"10\" bgcolor=\"$col\">\n".
+	"<TR bgcolor=\"$col\">\n".
 	"  <TD align=\"right\"><A NAME=\"%s\">%10s</A></TD>\n".
 	"  <TD align=\"right\">%11s</TD>\n".
 	"  <TD align=\"right\">%11s</TD>\n".
@@ -181,7 +194,7 @@ foreach $disk (sort keys %DINFO){
 	"  <TD>%s</TD>\n".
 	"  <TD align=\"right\">%s</TD>\n".
 	"</TR>\n",
-	    $tdisk,"<i><b>$disk</b></i>",$items[0],$items[1],$items[2],
+	    &GetRef($disk),"<i><b>$disk</b></i>",$items[0],$items[1],$items[2],
 	    $items[3],$items[4],$items[5];
 
     $totals[0] += $items[0];
@@ -195,7 +208,48 @@ foreach $disk (sort keys %DINFO){
 
 print $FO
     "</TABLE>\n",
-    "<B>Summary:</B><BR>\n",
+    "<P>",
+    "<H2><A NAME=\"PLOC\">Production Location</A></H2>\n",
+    "<TABLE align=\"center\" cellspacing=\"0\" border=\"1\" width=\"750\">\n",
+    "<TR>$TD Production $ETD$TD Trigger setup $ETD$TD Location list$ETD</TR>\n";
+
+undef(@LINES);
+foreach $tmp (sort keys %ALIBS){
+    @items = split(" ",$ALIBS{$tmp}); undef(%UD);
+    foreach $disku (sort(@items)){ 
+	($trg,$disk) = split(";",$disku);
+	if ( ! defined($TR{$tmp.$trg}) ){
+	    if ($#LINES != -1){ 
+		push(@LINES,"</TD>\n</TR>\n");
+		# make a separator at each new library
+		if ( ! defined($LB{$tmp}) ){
+		    push(@LINES,"<TR BGCOLOR=\"#333333\" HEIGHT=\"1\"><TD COLSPAN=\"3\">".
+			 "</TD></TR>\n");
+		}
+	    }
+	    push(@LINES,
+		 "<TR>\n",
+		 "\t<TD BGCOLOR=\"#DDDDDD\"><FONT FACE=\"Arial, Helvetica\">".
+		 (defined($LB{$tmp})?"&nbsp;":"<A NAME=\"".&GetRef($tmp)."\">$tmp</A>")."</FONT></TD>\n".
+		 "\t<TD BGCOLOR=\"#EEEEEE\">$trg</TD>\n".
+		 "\t<TD BGCOLOR=\"#EEEEEE\">");
+	    $TR{$tmp.$trg} = 1;
+	    $LB{$tmp}      = 1;
+	}
+	if ( ! defined($UD{$tmp.$trg.$disk}) ){
+	    push(@LINES,"<A HREF=\"#".&GetRef($disk)."\">$disk</A> ");
+	    $UD{$tmp.$trg.$disk} = 1;
+	}
+    }
+}
+push(@LINES,"</TD>\n</TR>\n");
+foreach $tmp (@LINES){  print $FO $tmp;}
+
+
+print $FO
+    "</TABLE>\n",
+    "<P>",
+    "<H2><A NAME=\"SUM\">Summary</A></H2>\n",
     "<BLOCKQUOTE>\n",
     "Total Space = ".sprintf("%.2f",$totals[0]/1024/1024)." GB<br>\n",
     "Total Used =  ".sprintf("%.2f",$totals[1]/1024/1024)." GB<br>\n",
@@ -218,3 +272,13 @@ if ( defined($ARGV[0]) ){
     rename("$ARGV[0]-tmp","$ARGV[0]");
 }
 
+
+
+
+sub GetRef
+{
+    my($el)=@_;
+    
+    $el =~ s/[\/ ]/_/g;
+    $el;
+}
