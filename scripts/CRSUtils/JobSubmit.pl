0835a4ade7918b8b7b74ded3da250436a79f6d52
didenko <>
2005-03-15 20:21:27 +0000

    add OSpaceZ2 in the  chain

--- scripts/CRSUtils/JobSubmit.pl
+++ scripts/CRSUtils/JobSubmit.pl
@@ -202,7 +202,7 @@ if ($ThisYear == 2002){
     # Default chain -- P2005 does not include Corr4 but Corr3
     $DCHAIN{"AuAu"}           = "P2005,svt_daq,svtD,EST,pmdRaw,Xi2,V02,Kink2,CMuDst,OShortR";
     $DCHAIN{"PPPP"}           = "P2005,ppOpt,svt_daq,svtD,EST,pmdRaw,Xi2,V02,Kink2,CMuDst,OShortR";
-    $DCHAIN{"CuCu"}           = "P2005,SCEbyE,OGridLeak,svt_daq,svtD,EST,pmdRaw,Xi2,V02,Kink2,CMuDst,OShortR";
+    $DCHAIN{"CuCu"}           = "P2005,OSpaceZ2,SCEbyE,OGridLeak,svt_daq,svtD,EST,pmdRaw,Xi2,V02,Kink2,CMuDst,OShortR";
 
     $DCALIB{"CuCu"}           = "OneSpaceCharge";
 
