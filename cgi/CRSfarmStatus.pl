bafb5f1ee8c1cabcdc86199755c8b93bff74079c
didenko <>
2005-10-24 18:28:33 +0000

    modified

--- cgi/CRSfarmStatus.pl
+++ cgi/CRSfarmStatus.pl
@@ -1,9 +1,12 @@
 #!/usr/local/bin/perl
 #!/usr/bin/env perl 
 #
-# $Id: CRSfarmStatus.pl,v 1.4 2005/10/19 21:04:44 didenko Exp $
+# $Id: CRSfarmStatus.pl,v 1.5 2005/10/24 18:28:33 didenko Exp $
 #
 # $Log: CRSfarmStatus.pl,v $
+# Revision 1.5  2005/10/24 18:28:33  didenko
+# modified
+#
 # Revision 1.4  2005/10/19 21:04:44  didenko
 # adjusted number of x marks
 #
@@ -29,25 +32,89 @@ use GIFgraph::linespoints;
 use GD;
 use Mysql;
 
+my @reqperiod = ("day","week","1_month","2_months","3_months","4_months","5_months","6_months");
+my @plotview = ("numbers","percentage");
+
 my $query = new CGI;
+
+#my $scriptname = $query->url(-relative=>1);
+
 
 my $day_diff = 1;
 my $max_y = 10000;
 my $min_y = 0;
 my @data;
 my @legend;
+my $Nmaxjobs = 422;
 
- my $fperiod   =  $query->param('period');
+ my $fperiod  =  $query->param('period');
+ my $plview   =  $query->param('plotvw');
 
 
-if ( ($fperiod eq "") ) {
-    print $query->header;
-    print $query->start_html('Farm Status for $fperiod');
-    print "<body bgcolor=\"cornsilk\"><center><pre>";
-    print "<h1>Youre query failed, try again!!</h1>";
-    print $query->end_html;
-    exit(0);
-}
+  if( $fperiod eq "" and $plview eq "") {
+
+
+print $query->header;
+print $query->start_html('CRS farm status');
+print <<END;
+<META HTTP-EQUIV="Expires" CONTENT="0">
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
+END
+print $query->startform(-action=>"CRSfarmStatus.dev.pl");  
+
+print "<body bgcolor=\"cornsilk\">\n";
+print "<h1 align=center><u>CRS farm status</u></h1>\n";
+print "<br>";
+print <<END;
+
+<hr>
+<table BORDER=0 align=center width=99% cellspacing=3>
+<tr ALIGN=center VALIGN=CENTER NOSAVE>
+<td>
+END
+
+print "<p>";
+print "</td><td>";
+print "<h3 align=center> Select period of monitoring</h3>";
+print "<h4 align=center>";
+print  $query->scrolling_list(-name=>'period',
+                             -values=>\@reqperiod,
+                             -default=>day,
+                             -size =>1); 
+
+print "<p>";
+print "</td><td>";
+print "<h3 align=center> How do you want to view plots:</h3>";
+print "<h4 align=center>";
+print  $query->scrolling_list(-name=>'plotvw',
+                             -values=>\@plotview,
+                             -default=>numbers,
+                             -size =>1); 
+
+
+print "<p>";
+print "</td> </tr> </table><hr><center>";
+
+print "</h4>";
+print "<br>";
+print "<br>";
+print "<br>";
+print $query->submit,"<p>";
+print $query->reset;
+print $query->endform;
+print "<br>";
+print "<br>";
+print "<address><a href=\"mailto:didenko\@bnl.gov\">Lidia Didenko</a></address>\n";
+
+print $query->end_html;
+
+  }else{
+
+my $qqr = new CGI;
+
+my $fperiod   =  $qqr->param('period');
+my $plview    =  $qqr->param('plotvw');
 
 my @numjobs1 = ();
 my @numjobs2 = ();
@@ -90,11 +157,15 @@ my @prt = ();
  @numjobs2 = ();
  @numjobs3 = ();
  @numjobs4 = ();
- @numjobs5 = ();
+ @jobrate1 = ();
+ @jobrate2 = ();
+ @jobrate3 = ();
+ @jobrate4 = ();
  @Npoint = ();
  @maxvalue = ();
 
 
+ 
  	if ($fperiod eq "day") {
 	    $sql="SELECT max(executing), max(importWaiting), max(exportWaiting), max(error) FROM  $crsJobStatusT WHERE sdate LIKE \"$nowdate%\" ";
         }else {
@@ -135,7 +206,14 @@ my @prt = ();
 
     &StcrsdbDisconnect();
 
-    @data = (\@Npoint, \@numjobs1, \@numjobs2, \@numjobs3, \@numjobs4 );
+   $graph = new GIFgraph::linespoints(750,650);
+
+ if ( ! $graph){
+    print STDOUT $qqr->header(-type => 'text/plain');
+    print STDOUT "Failed\n";
+ } else {
+    print STDOUT $qqr->header(-type => 'image/gif');
+    binmode STDOUT;
 
     $legend[0] = "Jobs in status 'executing'";
     $legend[1] = "Jobs waiting transferring from HPSS";
@@ -143,27 +221,8 @@ my @prt = ();
     $legend[3] = "Jobs failed";
 
 
-  $graph = new GIFgraph::linespoints(750,650);
-
-if ( ! $graph){
-    print STDOUT $query->header(-type => 'text/plain');
-    print STDOUT "Failed\n";
-} else {
-    print STDOUT $query->header(-type => 'image/gif');
-    binmode STDOUT;
-
-    my  $ymax = 1;
-
-    for ($k = 0; $k < scalar(@maxvalue); $k++) {
-	if( $ymax <= $maxvalue[$k]) {
-     $ymax = $maxvalue[$k];    
-       }
-    }
-
-
-  $min_y = 0;
-  $max_y = $ymax + 200 ;  
-
+ my $ylabel;
+ my $gtitle; 
  my $xLabelsVertical = 1;
  my $xLabelPosition = 0;
  my $xLabelSkip = 1;
@@ -176,10 +235,46 @@ $xLabelSkip = 60 if( $fperiod eq "3_months" );
 $xLabelSkip = 80 if( $fperiod eq "4_months" );
 $xLabelSkip = 120 if( $fperiod eq "5_months" );
 $xLabelSkip = 144 if( $fperiod eq "6_months" );
+
+    my  $ymax = 1;
+
+    for ($k = 0; $k < scalar(@maxvalue); $k++) {
+	if( $ymax <= $maxvalue[$k]) {
+     $ymax = $maxvalue[$k];    
+       }
+    }
+
+    if( $plview eq "numbers") {
+
+    @data = (\@Npoint, \@numjobs1, \@numjobs2, \@numjobs3, \@numjobs4 );
+
+  $min_y = 0;
+  $max_y = $ymax + 200 ;  
+  $ylabel = "Number of jobs";
+  $gtitle = "Number of jobs on the farm for the period of $fperiod ";
+
+    } else{
+
+  for ($i = 0; $i<scalar(@numjobs1); $i++) {
+  $jobrate1[$i] = $numjobs1[$i]*100/$Nmaxjobs; 
+  $jobrate2[$i] = $numjobs2[$i]*100/$Nmaxjobs;
+  $jobrate3[$i] = $numjobs3[$i]*100/$Nmaxjobs;
+  $jobrate4[$i] = $numjobs4[$i]*100/$Nmaxjobs;    
+ }
  
+    @data = (\@Npoint, \@jobrate1, \@jobrate2, \@jobrate3, \@jobrate4 );
+
+  $min_y = 0;
+  $max_y = $ymax*100/$Nmaxjobs + 40 ;  
+
+  $ylabel = "Number of jobs in % "; 
+  $gtitle = "Number of jobs on the farm in % to available slots for the period of $fperiod ";
+
+}
+
     $graph->set(x_label => "  ",
-		y_label => "Number of jobs",
-		title   => "Number of jobs on the farm for the period of $fperiod ",
+		y_label => $ylabel,
+		title   => $gtitle,
 		y_tick_number => 10,
                 x_label_position => 0.5,
 		y_min_value => $min_y,
@@ -195,6 +290,7 @@ $xLabelSkip = 144 if( $fperiod eq "6_months" );
                 x_labels_vertical =>$xLabelsVertical, 		
 		);
 
+
     $graph->set_legend(@legend);
     $graph->set_legend_font(gdMediumBoldFont);
     $graph->set_title_font(gdLargeFont);
@@ -204,7 +300,7 @@ $xLabelSkip = 144 if( $fperiod eq "6_months" );
     $graph->set_y_axis_font(gdMediumBoldFont);
     print STDOUT $graph->plot(\@data);
 }
-
+}
 
 sub y_format
 {
