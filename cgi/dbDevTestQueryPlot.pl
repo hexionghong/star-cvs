e2cd3fb0ab69b19d4cd3dc549e18af75dd25aefa
didenko <>
2006-07-21 18:55:44 +0000

    more fixes

--- cgi/dbDevTestQueryPlot.pl
+++ cgi/dbDevTestQueryPlot.pl
@@ -1,11 +1,11 @@
 #!/usr/local/bin/perl
 #!/usr/bin/env perl 
 #
-# $Id: dbDevTestQueryPlot.pl,v 1.33 2006/07/21 18:52:46 didenko Exp $
+# $Id: dbDevTestQueryPlot.pl,v 1.34 2006/07/21 18:55:44 didenko Exp $
 #
 # $Log: dbDevTestQueryPlot.pl,v $
-# Revision 1.33  2006/07/21 18:52:46  didenko
-# fixed syntax
+# Revision 1.34  2006/07/21 18:55:44  didenko
+# more fixes
 #
 # Revision 1.30  2006/04/14 16:20:23  didenko
 # updated for tracks with nfit point > 15
@@ -175,17 +175,17 @@ while($n_weeks >= 0) {
 	$path =~ s(/)(%)g;
 
 	if ($n_weeks == 0) {
-	    $sql="SELECT path, $mplotVal FROM JobStatus WHERE path LIKE \"%?%\" AND avail='Y' AND jobStatus=\"Done\" AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) < ? ORDER by createTime DESC LIMIT 5";
+	    $sql="SELECT path, $mplotVal FROM JobStatus WHERE path LIKE \"%$path%\" AND avail='Y' AND jobStatus=\"Done\" AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) < ? ORDER by createTime DESC LIMIT 5";
 
  	$cursor = $dbh->prepare($sql) || die "Cannot prepare statement: $dbh->errstr\n";
-	$cursor->execute($path, $day_diff);
+	$cursor->execute($day_diff);
 
 	} else {
-	    $sql="SELECT path, $mplotVal FROM JobStatus WHERE path LIKE \"%?%\" AND jobStatus=\"Done\" AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) < ? AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) > ? ORDER by createTime DESC LIMIT 5";
+	    $sql="SELECT path, $mplotVal FROM JobStatus WHERE path LIKE \"%$path%\" AND jobStatus=\"Done\" AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) < ? AND (TO_DAYS(\"$nowdate\") -TO_DAYS(createTime)) > ? ORDER by createTime DESC LIMIT 5";
 
 
 	$cursor = $dbh->prepare($sql) || die "Cannot prepare statement: $dbh->errstr\n";
-	$cursor->execute($path, $day_diff, $day_diff1);
+	$cursor->execute($day_diff, $day_diff1);
 
  }
 	while(@fields = $cursor->fetchrow_array) {
