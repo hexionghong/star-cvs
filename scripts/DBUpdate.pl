e45325ffa7b512fd8fc1527ede9d7c3f6f666d4c
jeromel <>
2008-03-31 17:56:12 +0000

    missing operator fix

--- scripts/DBUpdate.pl
+++ scripts/DBUpdate.pl
@@ -311,10 +311,10 @@ foreach  $file (@ALL){
     }
 
     # Add hook file which will globally leave
-    if (( -e $ENV{HOME}"/$SELF.quit" ||  -e "$CHKDIR/$SELF.quit") && 
+    if (( -e $ENV{HOME}."/$SELF.quit" ||  -e "$CHKDIR/$SELF.quit") && 
 	! defined($ENV{SPDR_DEBUG}) ){
-	my($f)=((-e $CHKDIR/$SELF.quit)?"$CHKDIR/$SELF.quit":$ENV{HOME}"/$SELF.quit");
-	print $FO "Warning : $f is present. Leaving\n";
+	my($f)=((-e "$CHKDIR/$SELF.quit")?"$CHKDIR/$SELF.quit":$ENV{HOME}."/$SELF.quit");
+	print $FO "Warning :  $f is present. Leaving\n";
 	last;
     }
 
