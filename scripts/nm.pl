f5058fd59ddea2c80cedd5bd98b502f470030bee
jeromel <>
2009-11-23 20:52:56 +0000

    Add user local libs&files if found Change the search method to eliminate the infamous "nm: no symbols" Some minor formatting issues (align symbol explaination) Add 'R' section

--- scripts/nm.pl
+++ scripts/nm.pl
@@ -3,7 +3,7 @@
 #
 # Usage: nm.pl SymbolToLocate [LibraryPattern]
 #
-# (c) J.Lauret 2003-2008
+# (c) J.Lauret 2003-2009
 #
 
 $STAR       = $ENV{STAR};
@@ -16,6 +16,17 @@ $OSG        = $ENV{OSG};
 if ( !defined($ARGV[0]) ){
     die "Syntax: $0 SymbolToLocate [LibraryPattern]\n";
 }
+
+# try to locate nm
+$NM = "";
+foreach ( ("/usr/bin/nm","/bin/nm")){
+    if ( -x $_ ){
+	$NM = $_;
+	last
+    }
+}
+$NM ="nm" if ($NM eq "");
+
 
 # else ...
 $symb = $ARGV[0];
@@ -35,14 +46,16 @@ if($pat !~ /\./){ $pat .= ".so";}
 # Defines the directory to look-up.
 # There is a STAR dependency here ...
 #
+$LIBL = "lib"; # default
 if( ! defined($ARGV[2]) ){
     if ( defined($ENV{INSURE}) ) {
-	$dir = "$STAR/.$OS/ILIB";
+	$LIBL= "ILIB";
     } elsif ( defined($ENV{NODEBUG}) ){
-	$dir = "$STAR/.$OS/LIB";
+	$LIBL = "LIB";
     } else {
-	$dir = "$STAR/.$OS/lib";
+	$LIBL = "lib";
     }
+    $dir = "$STAR/.$OS/$LIBL";
 } else {
     $dir = $ARGV[2];
 }
@@ -62,54 +75,89 @@ print "Searching for   $symb = $CXXFILT\n";
 
 print "\tLooking into $dir/$pat\n";
 push(@all,glob("$dir/$pat"));
-    
-print "\tAdding files from $ROOTSYS/lib/$pat\n";
-push(@all,glob("$ROOTSYS/lib/$pat"));
+
+# Match LIBL pattern if found, revert to "lib" otherwise
+if ( -e  "$ROOTSYS/$LIBL"){
+    $rs = "$ROOTSYS/$LIBL/$pat";
+} else {
+    $rs = "$ROOTSYS/lib/$pat";
+}
+print "\tAdding files from $rs\n";
+push(@all,glob("$rs"));
+
+# Also add user's directory
+if ( -e  ".$OS/$LIBL"){
+    print "\tAdding files from .$OS/$LIBL/$pat\n";
+    push(@all,glob(".$OS/$LIBL/$pat"));
+}
+
+
 
 print "\tAdding files from $CERN/$CERN_LEVEL/lib/*.a\n";
 push(@all,glob("$CERN/$CERN_LEVEL/lib/*.a"));
+
 
 if ( defined($OSG) ){
     print "\tAdding Globus librraies from $OSG/globus/lib/*.so\n";
     push(@all,glob("$OSG/globus/lib/*.so"));
 }
 
+# a common file intermediate
+$FF = "/tmp/nm$$.$<.tmp";
+
 foreach $file (@all){
-    chomp(@res = `nm $file | /bin/grep $symb`);
+    unlink($FF) if ( -e $FF);
+    `$NM $file >&$FF`;
+    if ( -e $FF ){
+	open(FI,"<$FF"); @SYM = <FI>; close(FI);
+	chomp(@res = grep(/$symb/,@SYM));
+    }
+    
+    # chomp(@res = `$NM $file >&$FF`); 
+    # | /bin/grep $symb`);
+
     if($#res != -1){
 	print "$file >>\n";
 	foreach $el (@res){
+	    $l   = length($el);
+	    $c   = 80-$l;
+	    $pad = "";
+	    for ( ; $c >= 0 ; $c--){  $pad .= " ";}
+
 	    if ($el =~ /(\s+A )(.*)/){
-		print "$el   \t[Absolute symbol]\n";		
+		print "$el$pad [Absolute symbol]\n";		
 	    } elsif ($el =~ /(\s+B )(.*)/){		
-		print "$el   \t[bss (unitialized data space)]\n";
+		print "$el$pad [bss (unitialized data space)]\n";
 	    } elsif ($el =~ /(\s+C )(.*)/){		
-		print "$el   \t[COMMON symbol]\n";
+		print "$el$pad [COMMON symbol]\n";
 	    } elsif ($el =~ /(\s+D )(.*)/){		
-		print "$el   \t[Data object symbol]\n";
+		print "$el$pad [Data object symbol]\n";
 	    } elsif ($el =~ /(\s+F )(.*)/){		
-		print "$el   \t[File symbol]\n";
+		print "$el$pad [File symbol]\n";
 	    } elsif ($el =~ /(\s+G )(.*)/){		
-		print "$el   \t[Small data object symbol (optimized access)]\n";
+		print "$el$pad [Small data object symbol (optimized access)]\n";
 	    } elsif ($el =~ /(\s+I )(.*)/){		
-		print "$el   \t[Indirect reference (GNU extension)]\n";		
+		print "$el$pad [Indirect reference (GNU extension)]\n";		
 	    } elsif ($el =~ /(\s+N )(.*)/){		
-		print "$el   \t[Symbol has no type or is a debugging information]\n";
+		print "$el$pad [Symbol has no type or is a debugging information]\n";
 	    } elsif ($el =~ /(\s+L )(.*)/){		
-		print "$el   \t[Thread-Local storage symbol]\n";
+		print "$el$pad [Thread-Local storage symbol]\n";
+	    } elsif ($el =~ /(\s+R )(.*)/){		
+		print "$el$pad [Read only data section]\n";
 	    } elsif ($el =~ /(\s+S )(.*)/){		
-		print "$el   \t[Section symbol for small objects]\n";
+		print "$el$pad [Section symbol for small objects]\n";
 	    } elsif ($el =~ /(\s+T )(.*)/){		
-		print "$el   \t[Text symbol or code section (defined)]\n";
+		print "$el$pad [Text symbol or code section (defined)]\n";
 	    } elsif ($el =~ /(\s+U )(.*)/){
-		print "$el   \t[Undefined symbol (MUST be resolved))]\n";		
+		print "$el$pad [Undefined symbol (MUST be resolved))]\n";		
 	    } elsif ($el =~ /(\s+V )(.*)/){
-		print "$el   \t[Weak object symbol]\n";	
+		print "$el$pad [Weak object symbol]\n";	
 	    } elsif ($el =~ /(\s+W )(.*)/){
-		print "$el   \t[Implicit weak object symbol (untagged)]\n";						
+		print "$el$pad [Implicit weak object symbol (untagged)]\n";
 	    } else {
 		print "$el\n";
 	    }
 	}
     }
 }
+unlink($FF) if ( -e $FF);
