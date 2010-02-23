1693059a5e38095d51990407c3d8e30f6fe4af81
didenko <>
2010-02-23 19:51:22 +0000

    more improvement

--- cgi/jobChainRetrv.pl
+++ cgi/jobChainRetrv.pl
@@ -13,15 +13,11 @@ BEGIN {
 
 use CGI;
 
-# &cgiSetup();
-
  my $q=new CGI;
 
  if ( exists($ENV{'QUERY_STRING'}) ) { print $q->header };
 
-
 my $set = $q->param("set");
-
 
 &beginHtml();
 
@@ -31,12 +27,12 @@ sub beginHtml {
 print <<END;
 <html>
   <head>
-          <title>Chain used for test</title>
+          <title>Chain options</title>
   </head>
   <body BGCOLOR=\"cornsilk\"> 
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 >
 <TR>
-<TD  ALIGN=CENTER WIDTH=\"100%\" HEIGHT=50><h3>Chain options</h3></TD>
+<TD  ALIGN=CENTER WIDTH=\"100%\" HEIGHT=50><h3>Chain options used for test</h3></TD>
 </TR><TR>
 <TD  ALIGN=CENTER WIDTH=\"100%\" HEIGHT=50><h3>$set</h3></TD>
 </TR>
@@ -47,20 +43,3 @@ print <<END;
 END
 }
 
-###############
-sub endHtml {
-my $Date = `date`;
-
-print <<END;
-</TABLE>
-   </body>
-</html>
-END
-
-}
-
-##############
-sub cgiSetup {
-    $q=new CGI;
-    if ( exists($ENV{'QUERY_STRING'}) ) { print $q->header };
-}
