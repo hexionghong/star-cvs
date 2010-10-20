955acdbe1cc4c51bab508c5693577bbe22859003
didenko <>
2010-10-20 15:50:08 +0000

    updated title

--- cgi/CRSqueueStatus.pl
+++ cgi/CRSqueueStatus.pl
@@ -1,10 +1,10 @@
 #!/usr/local/bin/perl
 #!/usr/bin/env perl 
 #
-# $Id: CRSqueueStatus.pl,v 1.16 2010/10/20 15:48:11 didenko Exp $
+# $Id: CRSqueueStatus.pl,v 1.17 2010/10/20 15:50:08 didenko Exp $
 #
 # $Log: CRSqueueStatus.pl,v $
-# Revision 1.16  2010/10/20 15:48:11  didenko
+# Revision 1.17  2010/10/20 15:50:08  didenko
 # updated title
 #
 # Revision 1.15  2010/10/20 15:36:25  didenko
@@ -309,7 +309,7 @@ my @prt = ();
       }
    $min_y = 0;
   $max_y = 140;       
-  $ylabel = "Number of jobs in the queue in % ";
+  $ylabel = "Number of jobs in the queue in % to max number of slots";
   $gtitle = "Number of jobs in the queues in % to max slots in the queue for period of $fperiod ";
 
     }
