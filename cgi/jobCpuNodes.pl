f34a7d0508ce04ac50d5e8635a79b6891e44f4a7
didenko <>
2011-03-30 18:04:31 +0000

    slightly modified

--- cgi/jobCpuNodes.pl
+++ cgi/jobCpuNodes.pl
@@ -784,7 +784,6 @@ my @arnode = ( "rcrs6114.rcf.bnl.gov",
   @data = (\@ndate, \@arphysics, \@argamma, \@arhlt, \@arht, \@armonitor, \@arpmdftp, \@arupc, \@aratomcules, \@armtd ) ;
 
   	$max_y = $maxval + 0.2*$maxval; 
-#        $max_y = int($max_y);
 
   }elsif(  $qvalue eq "cpu" ) {
 
