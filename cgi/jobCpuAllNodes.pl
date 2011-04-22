540f21fbc78d66952b056e86a52494284cb8b46b
didenko <>
2011-04-22 19:04:11 +0000

    syntax fixed

--- cgi/jobCpuAllNodes.pl
+++ cgi/jobCpuAllNodes.pl
@@ -20,8 +20,8 @@ use GD::Graph::linespoints;
 use Mysql;
 use Class::Struct;
 
-#$dbhost="fc2.star.bnl.gov:3386";
-$dbhost="duvall.star.bnl.gov";
+$dbhost="fc2.star.bnl.gov:3386";
+#$dbhost="duvall.star.bnl.gov";
 $dbuser="starreco";
 $dbpass="";
 $dbname="operation";
