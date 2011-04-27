64f070c9a8cfd88f5cc4a4b8c01adb70a3557144
didenko <>
2011-04-27 20:57:58 +0000

    change title

--- cgi/jobCpuAllNodes.pl
+++ cgi/jobCpuAllNodes.pl
@@ -512,7 +512,7 @@ my $qnode   = $qqr->param('pnode');
        if ( $qvalue eq "rtime/cpu" ) {
 
        $ylabel = "Average ratio RealTime/CPU";
-       $gtitle = "Average ratio RealTime/CPU for $qnode nodes and $qday for different streams";
+       $gtitle = "Ratio RealTime/CPU for $qnode nodes and $qday for different streams";
 
     @data = ();
 
@@ -523,7 +523,7 @@ my $qnode   = $qqr->param('pnode');
   }elsif(  $qvalue eq "cpu" ) {
 
        $ylabel = "Average CPU in sec/evt ";
-       $gtitle = "Average CPU in sec/evt for  $qnode nodes and $qday for different streams";
+       $gtitle = "CPU in sec/evt for  $qnode nodes and $qday for different streams";
 
     @data = ();
 
