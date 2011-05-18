56ee80df9ca2b467b29c6b4c44b64091e4a80ca8
didenko <>
2011-05-18 17:29:30 +0000

    fix path

--- cgi/NewLibPlots.pl
+++ cgi/NewLibPlots.pl
@@ -262,12 +262,9 @@ my $dpth;
  @spl = split("/", $pth);
  $path = $spl[1]."/". $spl[2];
  $dpth = $spl[0]; 
-# $dpth =~ s/_/%/g;      
-  @spl = ();
-  @spl = split("_", $dpth); 
- $dpth = $spl[0]; 
+ $dpth =~ s/_/%/g;      
  
- my $qupath = "%new/".$dpth."%/".$path;
+ my $qupath = "%new/".$dpth."/".$path;
 
 @plotvaldg = ();
 @plotvalop = ();
