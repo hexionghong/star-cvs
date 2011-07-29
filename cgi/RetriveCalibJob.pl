e59b25f8286e0e08b2f13522ea3dd503f2539e9b
didenko <>
2011-07-29 18:47:16 +0000

    more fixes

--- cgi/RetriveCalibJob.pl
+++ cgi/RetriveCalibJob.pl
@@ -286,6 +286,7 @@ print <<END;
 <TD ALIGN=CENTER WIDTH=\"50%\" HEIGHT=60><B><h3>Jobfilename</h3></B></TD>
 <TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Job status</h3></B></TD>
 <TD ALIGN=CENTER WIDTH=\"20%\" HEIGHT=60><B><h3>No.events produced</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"20%\" HEIGHT=60><B><h3>Avg.No.tracks</h3></B></TD>
 </TR>
    </head>
     </body>
