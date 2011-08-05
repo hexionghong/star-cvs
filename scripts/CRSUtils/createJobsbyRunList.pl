e26540209a187299927689dbe702dc4aa2f906b2
didenko <>
2011-08-05 15:06:08 +0000

    updated script for jobs creation

--- scripts/CRSUtils/createJobsbyRunList.pl
+++ scripts/CRSUtils/createJobsbyRunList.pl
@@ -4,12 +4,12 @@
 # 
 #  
 # 
-# createJobsbyRunList.pl - scripts to create CRS jobs. Use 3 (4) arguments: production serie,
-# filename with list of run numbers, chainName from ProdOptions table and name of daq file
-# if requested.   
+# createJobsbyRunList.pl - scripts to create CRS jobs. Use 4 arguments: production serie,
+# filename with list of run numbers, chainName from ProdOptions table and stream name
+# (use "all" if all stream data should be processed)   
 # For example:
-#  createJobsbyRunList.pl P07ie runlist2007.list auau200.2007.prod2 st_btag
-# Run on rcrsuser3
+# createJobsbyRunList.pl P07ie runlist2007.list auau200.2007.prod2 all
+# 
 #
 # Author:  L.Didenko
 #
@@ -29,27 +29,16 @@ $dbuser="starreco";
 $dbpass="";
 $dbname="operation";
 
-$JobStatusT = "JobStatus2007";
+$JobStatusT = "JobStatus2011";
 $ProdOptionsT = "ProdOptions";
 
+my $prodPeriod = $ARGV[0]; 
+my $chName = $ARGV[1];
+my $fileName = $ARGV[2]; 
+my $ftype = $ARGV[3];
 
-my $debugOn=0;
-
-my $prodPeriod = $ARGV[0];
-my $fileName = $ARGV[1]; 
+my $datDisk = "/star/data+20-90";             
 my $trig;
-my $chName = $ARGV[2];
-my $datDisk = "/star/data+12-55";             
-my $ftype = "";
-my $coll;
-my $chainDir = "daq";
-
-   if ( !defined $ARGV[3]) {
-   $ftype = "";
-   }else{
-    $ftype = $ARGV[3]; 
-   }
-
 
 my $listName = "/star/u/starreco/".$fileName;
 
@@ -59,15 +48,13 @@ my @runList = ();
 
  @runList = <RUNLIST>;
 
-
-
 ###Set directories to be created for jobfiles
 
 my $DISK1        = "/star/rcf/prodlog/";
 my $TOPHPSS_SINK = "/home/starsink/raw/daq";
 my $TOPHPSS_RECO = "/home/starreco/reco";
-my $JOB_LOG =  $DISK1 . $prodPeriod . "/log/" . $chainDir ;
-my $JOB_DIR;
+my $JOB_LOG =  $DISK1 . $prodPeriod . "/log/daq" ;
+my $JOB_DIR =  "/star/u/starreco/" . $prodPeriod ."/requests/daq"; 
 
 my @jobs_set = ();
 
@@ -78,27 +65,27 @@ my @jobs_set = ();
  my $jb_news;
  my $jb_archive;
  my $jb_jobfile;
- my $jb_hold;
  my $jb_fstat;
+ my $jb_jobloop;
+ my $jb_joblost;
+
 
 ########  declare variables needed to fill the JobStatus table
-
- my $mchainOp;
 
  my $mjobID = "n/a";
  my $mprodSr = "n/a";
  my $mjobFname = "n/a";
  my $mjobFdir = "n/a";
- my $msumFile = "n/a";
- my $msumDir = "n/a";
+ my $mlogFile = "n/a";
+ my $mlogDir = "n/a";
  my $mjobSt = "n/a";
  my $startId = "Job_P00h";
  my $startSer = "P00h";
  my $new_id = 0;
  my $mchain; 
  my $mlibVer; 
-
- $JOB_DIR =  "/home/starreco/newcrs/" . $prodPeriod ."/requests/". $chainDir; 
+ my $hpssSt = 'n/a';
+ my $outhpss = 'n/a';
 
  &StDbProdConnect();
 
@@ -111,7 +98,7 @@ my @jobs_set = ();
     $rv = $dbh->do($sql) || die $dbh->errstr;
     $new_id = $dbh->{'mysql_insertid'};
 
-#############################################################################
+############################################################################
 
 
  $sql="SELECT libVersion, chainOpt FROM $ProdOptionsT where prodSeries = '$prodPeriod' AND chainName = '$chName'";
@@ -129,6 +116,8 @@ my @jobs_set = ();
 
       print "$fname = $fvalue\n" if $debugOn;
 
+#      print "$fname = $fvalue\n";
+
          $mchain   = $fvalue    if( $fname eq 'chainOpt');          
          $mlibVer  = $fvalue    if( $fname eq 'libVersion');   
        }
@@ -137,7 +126,7 @@ my @jobs_set = ();
      &StDbProdDisconnect();
 
 
-my $SITE = "BNL";
+ my $SITE = "BNL";
 
 my $fileC = new FileCatalog();
 
@@ -148,29 +137,32 @@ my @prt = ();
 my $flname;
 my $jpath;
 my $jfile;
-my $chain;
 my $num = 0;
 my $nfiles = 0;
 my $fileSeq;
 my $field;
 my $myID;
-
+my $mrunId;
+my $strName; 
+my @flsplit = ();
 
  for ($ii=0; $ii< scalar(@runList); $ii++)  {
 
- $nfiles = 0;
- 
  chop $runList[$ii];
+ @jobs_set = ();
+ $nfiles = 0; 
+ $mrunId = $runList[$ii];
 
-#  print "Run number to be processed:  ",$runList[$ii], "\n";
+  print "Run number to be processed:  ",$runList[$ii], "\n";
  
-  if( $ftype eq "") {
+  if( $ftype eq "all") {
 
-  $fileC->set_context("runnumber=$runList[$ii]","tpc=1","filetype=online_daq","sanity=1","storage=HPSS");
+  $fileC->set_context("runnumber=$runList[$ii]","filetype=online_daq","sanity=1","storage=HPSS");
    }else{
   
-  $fileC->set_context("runnumber=$runList[$ii]","tpc=1","filetype=online_daq","filename~$ftype","sanity=1","storage=HPSS");
+  $fileC->set_context("runnumber=$runList[$ii]","filetype=online_daq","filename~$ftype","sanity=1","storage=HPSS");
   }
+
   @jobs_set = $fileC->run_query("trgsetupname","path","filename","fileseq","magscale");
 
     $fileC->clear_context();
@@ -180,6 +172,7 @@ my $myID;
 
   foreach my $jobline (@jobs_set){
  
+    @flsplit = ();
     @prt = ();
     @prt = split("::",$jobline);
 
@@ -190,52 +183,51 @@ my $myID;
     $field = $prt[4]; 
     $jfile = $flname;
     $jfile =~ s/.daq//g;
+    @flsplit = split ("_",$jfile);   
+    $strName = $flsplit[1];
 
 #    print $trig,"   ",$jpath,"   ",$field,"   ",$flname,"\n";
-
-       next if ($field eq "FieldOff");
 
     $num = $fileSeq/10 - int($fileSeq/10);
            $mprodSr = $prodPeriod; 
            $myID = 100000000 + $new_id;
            $mjobID = "Job". $myID . "/" . $prodPeriod ."/". $mlibVer;
-           $msumFile = $jfile . ".log";
-           $msumDir = $JOB_LOG;
+           $mlogFile = $jfile . ".log";
+           $mlogDir = $JOB_LOG;
            $mjobFdir = "new_jobs";
            $mjobSt = "n/a";
-           $mchName = $chName;
-           $chain = $mchain;
           @parts =  split ("/", $jpath);
-    $jbset = $trig . "_" .$field . "_" .$prodPeriod . "_" . $parts[5] . "_" . $parts[6]. "_" . $parts[7];
-   
-          $mjobFname = $trig . "_" .$field . "_" .$prodPeriod ."_". $jfile;
 
-    $jb_fstat = 1;
+    $jbset = $field . "_" .$prodPeriod . "_" . $parts[5] . "_" . $parts[6]. "_" . $parts[7];
+   
+    $mjobFname = $trig . "_" .$field . "_" .$prodPeriod ."_". $jfile;
+
+
+   $jb_fstat = 1;
     $jb_news = $JOB_DIR . "/new_jobs/" . $mjobFname;
     $jb_archive = $JOB_DIR . "/archive/" . $mjobFname;
     $jb_jobfile = $JOB_DIR . "/jobfiles/" . $mjobFname;
-    $jb_hold = $JOB_DIR . "/jobs_hold/" . $mjobFname;
+    $jb_jobloop = $JOB_DIR . "/jobs_looping/" . $mjobFname;
+    $jb_joblost = $JOB_DIR . "/jobs_lostfiles/" . $mjobFname;
      if (-f $jb_news)     {$jb_fstat = 0};
      if (-f $jb_archive)  {$jb_fstat = 0};
      if (-f $jb_jobfile)  {$jb_fstat = 0};
-     if (-f $jb_hold)     {$jb_fstat = 0};
+     if (-f $jb_jobloop)  {$jb_fstat = 0};
+     if (-f $jb_joblost)  {$jb_fstat = 0};
 
       if($jb_fstat == 1)  {
       
-      if($num == 0 ) {
-      $chain = $mchain .",-hitfilt";
-    }
-#     if($nfiles < 11 ) {
-          &create_jobs($jfile, $jbset, $chain, $mlibVer, $JOB_DIR, $datDisk); 
+          &create_jobs($jfile, $jbset, $mchain, $mlibVer, $JOB_DIR, $datDisk); 
 
       print "JOB ID = " ,$mjobID, " % " . $mjobFname,  "\n";
       $nfiles++;
 #####  fill  JobStatus table
       print "filling JobStatus table\n";
  
-     &fillJSTable();   
+#	  print $mjobID," % ", $mprodSr," % ", $trig ," % ",$mjobFname," % ",$strName," % ",$mrunId," % ",$mjobFdir," % ",$mlogFile," % ",$mlogDir," % ",$hpssSt," % ",$outhpss," % ",$mjobSt," % ",$chName, "\n";
 
-#      } 
+    &fillJSTable();   
+
      }
  }
 }
@@ -260,11 +252,16 @@ my $myID;
     $sql="insert into $JobStatusT set ";
     $sql.="jobID='$mjobID',";
     $sql.="prodSeries='$mprodSr',";
+    $sql.="trigsetName='$trig',";
     $sql.="jobfileName='$mjobFname',";
+    $sql.="streamName='$strName',";
+    $sql.="runID='$mrunId',";
     $sql.="jobfileDir='$mjobFdir',";
-    $sql.="sumFileName='$msumFile',";
-    $sql.="sumFileDir='$msumDir',";
-    $sql.="jobStatus='$mjobSt',"; 
+    $sql.="logfileName='$mlogFile',";
+    $sql.="logfileDir='$mlogDir',";
+    $sql.="inputHpssStatus='$hpssSt',";
+    $sql.="outputHpssStatus='$outhpss',";
+    $sql.="jobStatus='$mjobSt',";
     $sql.="chainName='$chName'";
 
      print "$sql\n" if $debugOn;
@@ -283,30 +280,29 @@ my $myID;
  my $Jsetd;
  my $Jsetr;
  my $inFile;
- my $logDir;
  my @pts = ();
 
     @pts = split ("_",$Jset);
-    $Jsetr = $pts[3] . "/" .$pts[4]. "/" .$pts[5];
-    $Jsetd = $pts[0] . "/" .$pts[1] . "/" . $pts[2] . "/" .$Jsetr;     
-    $inFile =  $gfile . ".daq";
-    $logDir = $JOB_LOG;   
+    $Jsetr = $pts[2]."/".$pts[3]."/".$pts[4];
+    $Jsetd = $trig."/".$pts[0]."/".$pts[1]."/".$Jsetr;     
+
+    $inFile = $gfile . ".daq";
 
    my $exArg = "29,".$jlibVer ."," .$dataDisk . ",-1," . $fchain;
   
-    my $jb_new = $JobDir . "/new_jobs/" . $pts[0] . "_" .$pts[1] . "_" . $pts[2]  . "_" . $gfile;
-  
+    my $jb_new = $JobDir . "/jobfiles/" . $trig."_".$pts[0]."_".$pts[1]."_".$gfile;
+
       my $hpss_raw_dir  = $TOPHPSS_SINK . "/" . $Jsetr;
       my $hpss_raw_file = $inFile;
       my $hpss_dst_dir  = $TOPHPSS_RECO . "/" . $Jsetd;
-      my $hpss_dst_file0 = $gfile . ".event.root";
+      my $hpss_dst_file0 = $gfile . ".MuDst.root";
       my $hpss_dst_file1 = $gfile . ".hist.root";
       my $hpss_dst_file2 = $gfile . ".tags.root";
-      my $hpss_dst_file3 = $gfile . ".MuDst.root";
+      my $hpss_dst_file3 = $gfile . ".event.root";
 #      my $hpss_dst_file4 = $gfile . ".runco.root";    
-      my $executable     = "/afs/rhic/star/packages/scripts/bfccb";
+      my $executable     = "/afs/rhic/star/packages/scripts/bfcca";
       my $executableargs = $exArg; 
-      my $log_dir       = $logDir;
+      my $log_dir       = $JOB_LOG;
       my $log_name      = $gfile . ".log";
       my $err_log       = $gfile . ".err";
       if (!open (JOB_FILE,">$jb_new")) {printf ("Unable to create job submission script %s\n",$jb_new);}
@@ -314,15 +310,15 @@ my $myID;
 
 
        print JOB_FILE "#input\n";
-       print JOB_FILE "      inputnumstreams=2\n";
+       print JOB_FILE "      inputnumstreams=1\n";
        print JOB_FILE "      inputstreamtype[0]=HPSS\n";
        print JOB_FILE "      inputdir[0]=$hpss_raw_dir\n";
        print JOB_FILE "      inputfile[0]=$hpss_raw_file\n";
-       print JOB_FILE "      inputstreamtype[1]=UNIX\n";
-       print JOB_FILE "      inputdir[1]=/star/data13/reco/StarDb\n";
-       print JOB_FILE "      inputfile[1]=OneSpaceCharge\n";
+#       print JOB_FILE "      inputstreamtype[1]=UNIX\n";
+#       print JOB_FILE "      inputdir[1]=/star/data15/reco/StarDb\n";
+#       print JOB_FILE "      inputfile[1]=OneSpaceCharge\n";
        print JOB_FILE "#output\n";
-       print JOB_FILE "      outputnumstreams=4\n";
+       print JOB_FILE "      outputnumstreams=3\n";
        print JOB_FILE "#output stream \n";
        print JOB_FILE "      outputstreamtype[0]=HPSS\n";
        print JOB_FILE "      outputdir[0]=$hpss_dst_dir\n";
@@ -333,19 +329,16 @@ my $myID;
        print JOB_FILE "      outputstreamtype[2]=HPSS\n";
        print JOB_FILE "      outputdir[2]=$hpss_dst_dir\n";
        print JOB_FILE "      outputfile[2]=$hpss_dst_file2\n";
-       print JOB_FILE "      outputstreamtype[3]=HPSS\n";
-       print JOB_FILE "      outputdir[3]=$hpss_dst_dir\n";
-       print JOB_FILE "      outputfile[3]=$hpss_dst_file3\n";
-#       print JOB_FILE "      outputstreamtype[4]=HPSS\n";
-#       print JOB_FILE "      outputdir[4]=$hpss_dst_dir\n";
-#       print JOB_FILE "      outputfile[4]=$hpss_dst_file4\n";
-       print JOB_FILE "#standard out -- Should be five outputs\n";
+#       print JOB_FILE "      outputstreamtype[3]=HPSS\n";
+#       print JOB_FILE "      outputdir[3]=$hpss_dst_dir\n";
+#       print JOB_FILE "      outputfile[3]=$hpss_dst_file3\n";
+       print JOB_FILE "#standard out -- Should be 3 outputs\n";
        print JOB_FILE "      stdoutdir=$log_dir\n";
        print JOB_FILE "      stdout=$log_name\n";
        print JOB_FILE "#standard error -- Should be four\n";
        print JOB_FILE "      stderrdir=$log_dir\n";
        print JOB_FILE "      stderr=$err_log\n";
-       print JOB_FILE "      notify=starreco\@rcrsuser3.rcf.bnl.gov\n";
+       print JOB_FILE "      notify=starreco\@rcrsuser1.rcf.bnl.gov\n";
        print JOB_FILE "#program to run\n";
        print JOB_FILE "      executable=$executable\n";
        print JOB_FILE "      executableargs=$executableargs\n";
