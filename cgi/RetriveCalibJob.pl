86cc2142ec8be3f3532940f8acde616a2983fb68
didenko <>
2011-08-10 15:38:30 +0000

    add names

--- cgi/RetriveCalibJob.pl
+++ cgi/RetriveCalibJob.pl
@@ -68,6 +68,7 @@ my @jbStatus = ();
 my @jbfName = ();
 my @jbEvent = ();
 my @disklst = ();
+my $diskname = ();
 my $nn = 0;
 my $nnd = 0;
 my $dnm = 0;
@@ -203,6 +204,7 @@ my $jobname = $qtrg."%".$qprod."%";
 
         while( $dnm = $cursor->fetchrow() ) {
           $disklst[$nnd] = $dnm;
+          $diskname[$nnd] = "/star/".$disklst[$nnd];
 
  print <<END;
 
@@ -396,7 +398,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qprod</font> production and <font color="blue">$qtrg </font> dataset location  </B></h2>
+ <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qprod</font> production of <font color="blue">$qtrg </font> dataset location  </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
