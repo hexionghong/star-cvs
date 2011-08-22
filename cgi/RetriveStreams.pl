73837a92aedc3db335b9a3f2998ab8891b1e6ed9
didenko <>
2011-08-22 20:33:50 +0000

    add time of creation

--- cgi/RetriveStreams.pl
+++ cgi/RetriveStreams.pl
@@ -29,7 +29,7 @@ if( $hour < 10) { $hour = '0'.$hour };
 if( $min < 10) { $min = '0'.$min };
 if( $sec < 10) { $sec = '0'.$sec };
 
-my $todate = ($year+1900)."-".$mon."-".$mday;
+my $todate = ($year+1900)."-".$mon."-".$mday." ".$hour.":".$min.":".$sec;
 
 my $SITE         = "BNL";
 my $status       = (0==1);
