0628ee9966eba110affd61c63744644e7db168ba
didenko <>
2011-12-14 16:01:57 +0000

    updates with disk names

--- cgi/RetriveCalibJob.pl
+++ cgi/RetriveCalibJob.pl
@@ -407,7 +407,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qprod</font> production of <font color="blue">$qtrg </font> dataset location  </B></h2>
+ <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qcalib</font> calibration production of <font color="blue">$qtrg </font> dataset </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
