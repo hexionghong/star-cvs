3f41de5094dfd16758d045d082cbf8d4ae086605
didenko <>
2011-12-29 20:44:06 +0000

    HPSS status by date

--- cgi/CRSHpssStatus_bydate.pl
+++ cgi/CRSHpssStatus_bydate.pl
@@ -30,7 +30,7 @@ $crsJobStatusT = "crsJobStatusY11";
 
   &StcrsdbConnect();
 
- $sql="SELECT DISTINCT  date_format(sdate, '%Y-%m-%d' ) as PDATE FROM $crsJobStatusT where sdate >= '2011-10-01 00:00:00' order by as PDATE " ;
+ $sql="SELECT DISTINCT  date_format(sdate, '%Y-%m-%d') as PDATE FROM $crsJobStatusT where sdate >= '2011-12-01 00:00:00' order by PDATE " ;
 
       $cursor =$dbh->prepare($sql)
           || die "Cannot prepare statement: $DBI::errstr\n";
@@ -308,7 +308,7 @@ $ymax = 1;
   $min_y = 0;
   $max_y = $ymax + 50 ;  
   $ylabel = "Number of failed jobs per hour";
-  $gtitle = "Number of jobs per hour failed to make HPSS transferring for the period of $fperiod ";
+  $gtitle = "Number of jobs per hour failed to make HPSS transferring for the period of $fperiod-$lperiod ";
 
     } else{
 
@@ -356,7 +356,7 @@ $ymax = 1;
 
 
   $ylabel = "Number of failed jobs in % to number of jobs finished per hour ";
-  $gtitle = "Number of failed jobs in % to number of jobs finished per hour for period of $fperiod ";
+  $gtitle = "Number of failed jobs in % to number of jobs finished per hour for period $fperiod to $lperiod ";
 
 }
 
