acc5c508105815d419b1dce8b7154ca5286acc3c
didenko <>
2012-02-13 20:33:53 +0000

    add tracker version to production tag

--- cgi/MCDataSummary.pl
+++ cgi/MCDataSummary.pl
@@ -88,6 +88,10 @@ my $prodname = "n/a";
     @prt = split("/",$recopath[$nlist]);
     $mcset[$nlist] = $prt[4]."/".$prt[5]."/". $prt[6]."/". $prt[7]."/".$prt[8]."/". $prt[9]; 
 
+    if($prod[$nlist] eq "eval" ) {
+       $prod[$nlist] = $prt[10];
+    }
+
      @runevents = ();
      $runevents[0] = 0;  
      @datasize = ();
