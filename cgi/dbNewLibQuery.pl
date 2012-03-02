5d68860a2acb3a89f625fe349243cc3df15769c8
didenko <>
2012-03-02 17:08:59 +0000

    added plots for new tests

--- cgi/dbNewLibQuery.pl
+++ cgi/dbNewLibQuery.pl
@@ -35,10 +35,13 @@ my $prepath;
 
 
 my @prod_set = (
+                "daq_sl302/year_2012/pp200_production_2012",
                 "daq_sl302/year_2011/AuAu200_production",
                 "daq_sl302/year_2011/AuAu27_production",
                 "daq_sl302/year_2011/AuAu19_production",
                 "daq_sl302/year_2011/pp500_production_2011",
+                "daq_sl302/year_2011/pp500_embed",
+                "daq_sl302/year_2011/AuAu200_embed",
                 "daq_sl302/year_2010/auau200_production",
                 "daq_sl302/year_2010/auau62_production",
                 "daq_sl302/year_2010/auau39_production",
@@ -71,6 +74,10 @@ my @prod_set = (
                 "daq_sl302/year_2001/ppMinBias",
                 "daq_sl302/year_2000/minbias",
                 "daq_sl302/year_2000/central",
+                "trs_sl302/year_2012/pp500_minbias",
+                "trs_sl302/year_2012/pp200_minbias",
+                "trs_sl302/year_2012/CuAu200_minbias",
+                "trs_sl302/year_2012/UU200_minbias",
                 "trs_sl302/year_2011/pp500_minbias",
                 "trs_sl302/year_2011/pp500_pileup", 
                 "trs_sl302/year_2010/auau200_minbias",
