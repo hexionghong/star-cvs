36ab493226bf59e54e4566001d2f1b3d8bb0e05a
didenko <>
2012-03-29 15:27:02 +0000

    add reference to production description

--- cgi/ProdTrgList.pl
+++ cgi/ProdTrgList.pl
@@ -513,15 +513,10 @@ print <<END;
  <h2 ALIGN=CENTER> <B> Real Data Production Summary  </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
-<TABLE ALIGN=CENTER>
-<TR>
-<TD ALIGN=LEFT><h4><font color="blue">Production descriptions can be found on  <a href="http://www.star.bnl.gov/public/comp/prod/ProdList.html"> the page</a></font></h4></TD>
-
-<TD ALIGN=RIGHT><h4><font color="#ff0000">Ongoing production is in red color</font></h4></TD>
-</TR>
-</TABLE>
+<h4 ALIGN=RIGHT><h4><font color="#ff0000">Ongoing production is in red color</font></h4>
 <h4 ALIGN=LEFT>Link under the trigger set name has stream data production summary <br>
-Link under production tag has chain options<br></h4>
+Link under production tag has chain options<br>
+<ALIGN=LEFT><h4><font color="blue">Production descriptions can be found on  <a href="http://www.star.bnl.gov/public/comp/prod/ProdList.html"> the page</a></font></h4>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
 <TR>
 <TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Trigger sets</h3></B></TD>
@@ -532,7 +527,6 @@ Link under production tag has chain options<br></h4>
 <TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Size (GB) of MuDst <h3></B></TD>
 <TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Number of MuDst files <h3></B></TD>
 </TR> 
-</TABLE>
    </head>
     </body>
 END
