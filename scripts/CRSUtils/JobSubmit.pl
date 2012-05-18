ae56e36697f009032e7322a565daf92e43441a50
jeromel <>
2012-05-18 14:53:58 +0000

    Case sensitive

--- scripts/CRSUtils/JobSubmit.pl
+++ scripts/CRSUtils/JobSubmit.pl
@@ -548,7 +548,7 @@ if ( $ThisYear == 2005 ){
 	$DCHAIN{"AuAu"} = "P2012a,AgML,mtdDat,btof,fmsDat,BEmcChkStat,Corr4,OSpaceZ2,OGridLeak3D,-hitfilt";
 	$DCHAIN{"PPPP"} = "pp2012a,AgML,mtdDat,btof,fmsDat,VFPPVnoCTB,beamline,BEmcChkStat,Corr4,OSpaceZ2,OGridLeak3D,-hitfilt";
 	$DCHAIN{"UU"}   = $DCHAIN{"AuAu"};
-	$DCHAIN{"CUAU"} = $DCHAIN{"AUCU"} =  $DCHAIN{"AuAu"}.",QAalltrigs";
+	$DCHAIN{"CuAu"} = $DCHAIN{"AuAu"}.",QAalltrigs";
     }
 
     # allow chain switch on condition matching
@@ -564,7 +564,7 @@ if ( $ThisYear == 2005 ){
     $SCALIB{"AuAu"}      = "OptLaser";
     $SCALIB{"PPPP"}      = "OptLaser";
     $SCALIB{"UU"}        = "OptLaser";
-    $SCALIB{"CUAU"}      = $SCALIB{"AUCU"} = "OptLaser";
+    $SCALIB{"CuAu"}      = "OptLaser";
 
 } else {
     # Well, at first you may get that message ... should tell you that
