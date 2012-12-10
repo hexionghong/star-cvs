dbfbc04406476cc1ac5c8bf9ec0f0e12f76251cc
didenko <>
2012-12-10 17:04:28 +0000

    minor changes

--- cgi/RetriveMCJob.pl
+++ cgi/RetriveMCJob.pl
@@ -270,7 +270,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\"> 
-<h2 ALIGN=CENTER> <B>No data for <font color="blue">$qtrg </font> dataset <font color="blue">$qprod</font> production <br> with  <font color="blue">$qreq </font> requestID  </B></h2>
+<h2 ALIGN=CENTER> <B>No data for <font color="blue">$qtrg </font> dataset <font color="blue">$qprod</font> production <br> with  requestID = <font color="blue">$qreq </font>  </B></h2>
     </body>
 END
 }
@@ -308,7 +308,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
-<h2 ALIGN=CENTER> <B>List of jobs failed to create output files on NFS in <br> <font color="blue">$qtrg </font>  dataset <br><font color="blue">$qprod</font> production  with <font color="blue">$qreq </font> requestID </B></h2>
+<h2 ALIGN=CENTER> <B>List of jobs failed to create output files on NFS in <br> <font color="blue">$qtrg </font>  dataset <br><font color="blue">$qprod</font> production  with requestID = <font color="blue">$qreq </font>  </B></h2>
  <h3 ALIGN=CENTER> Created on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
@@ -332,7 +332,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>Chain options for <font color="blue">$qtrg</font> <br> dataset production with <font color="blue"> $qreq </font> requestID </B></h2>
+ <h2 ALIGN=CENTER> <B>Chain options for <font color="blue">$qtrg</font> <br> dataset production with requestID = <font color="blue"> $qreq </font> </B></h2>
  <h3 ALIGN=CENTER> Created $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
