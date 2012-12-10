bc96c07b7e10ffb233f946399af3f7f9e87bce1f
didenko <>
2012-12-10 17:11:43 +0000

    minor changes

--- cgi/RetriveEmbJob.pl
+++ cgi/RetriveEmbJob.pl
@@ -360,7 +360,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qtrg</font> embedding production <br> for <font color="blue">$qpart </font> particle with <font color="blue">$qreq </font> requestID  </B></h2>
+ <h2 ALIGN=CENTER> <B>List of NFS disk names for <font color="blue">$qtrg</font> embedding production <br> for <font color="blue">$qpart </font> particle with  requestID = <font color="blue">$qreq </font> </B></h2>
  <h3 ALIGN=CENTER> Created on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
