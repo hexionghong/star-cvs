48647396c85ac5b304ff5664e470ce1e11196b3a
didenko <>
2012-12-10 17:22:35 +0000

    minor updates

--- cgi/RetriveCalibJob.pl
+++ cgi/RetriveCalibJob.pl
@@ -300,7 +300,7 @@ print <<END;
    <body BGCOLOR=\"cornsilk\"> 
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 >
 <TR>
- <TD ALIGN=CENTER> <B><h3> List of failed jobs for <font color="blue">$qcalib </font>calibration production <font color="blue"> $qprod </font><br>  for <font color="blue"> $qtrg </font>dataset </B></h3></TD>
+ <TD ALIGN=CENTER> <B><h3> List of failed jobs in <font color="blue">$qcalib </font>calibration production <font color="blue"> $qprod </font><br>  for <font color="blue"> $qtrg </font>dataset </B></h3></TD>
 </TR>
 </TABLE>
     </body>
@@ -316,7 +316,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>List of jobs crashed for <font color="blue">$qcalib </font>calibration  production <font color="blue"> $qprod </font><br> for <font color="blue">$qtrg </font> dataset  </B></h2>
+ <h2 ALIGN=CENTER> <B>List of jobs crashed in <font color="blue">$qcalib </font>calibration  production <font color="blue"> $qprod </font><br> for <font color="blue">$qtrg </font> dataset  </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
@@ -363,7 +363,7 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\">
- <h2 ALIGN=CENTER> <B>List of jobs 'hung' for <font color="blue">$qcalib </font>calibration production <font color="blue"> $qprod </font> <br> for <font color="blue">$qtrg </font> dataset  </B></h2>
+ <h2 ALIGN=CENTER> <B>List of jobs 'hung' in <font color="blue">$qcalib </font>calibration production <font color="blue"> $qprod </font> <br> for <font color="blue">$qtrg </font> dataset  </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
