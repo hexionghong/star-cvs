25af2c8958d42285b14a71756b3b43c5fb1f1ea7
didenko <>
2013-01-10 16:03:42 +0000

    minor updates

--- cgi/RetriveTrigID.pl
+++ cgi/RetriveTrigID.pl
@@ -111,9 +111,9 @@ print <<END;
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 >
 <TR>
-<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Trigger lable</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Offline trigger ID</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Number of Events</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=40><B><h3>Trigger lable</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=40><B><h3>Offline trigger ID</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=40><B><h3>Number of Events</h3></B></TD>
 
 </TR> 
    </head>
