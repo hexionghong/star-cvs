7a54c0bc919087fe465247de54fbeaff4a060e2b
didenko <>
2013-01-10 15:57:24 +0000

    modified

--- cgi/RetriveTrigID.pl
+++ cgi/RetriveTrigID.pl
@@ -14,9 +14,22 @@ BEGIN {
  use CGI::Carp qw(fatalsToBrowser carpout);
 }
 
+
+use CGI qw(:standard);
+use DBI;
+use Mysql;
+use Class::Struct;
+use Time::Local;
+
 use CGI;
-use lib "/afs/rhic.bnl.gov/star/packages/scripts";
-use FileCatalog;
+
+
+$dbhost="duvall.star.bnl.gov";
+$dbuser="starreco";
+$dbpass="";
+$dbname="operation";
+
+my $TrigDataT = "ProdTriggerSet";
 
 
 ($sec,$min,$hour,$mday,$mon,$year) = localtime();
@@ -31,54 +44,57 @@ if( $sec < 10) { $sec = '0'.$sec };
 
 my $todate = ($year+1900)."-".$mon."-".$mday." ".$hour.":".$min.":".$sec;
 
-my $SITE         = "BNL";
-my $status       = (0==1);
-
-my $fileC = new FileCatalog();
-
-    $fileC->connect_as($SITE."::User","FC_user") || die "Connection failed for FC_user\n";
 
 my @trglable = ();
 my @trgid = ();
+my @nevents = ();
 my $tline = 0;
 my $nlist = 0;
 
-my @artrig = ();
-my @prt = ();
-
 my $query = new CGI;
 
+ if ( exists($ENV{'QUERY_STRING'}) ) { print $query->header };
+
+
 my $qtrg = $query->param('trigs');
+my $qprod = $query->param('prods');
 
 $artrig[0] = 0;
 
 &beginHtml();
 
- $fileC->set_context("trgsetupname=$qtrg","filetype=online_daq","sanity=1","storage=hpss","limit=0");
+   &StDbProdConnect();
 
- @artrig = $fileC->run_query("trgname","orda(trgword)");
+   $sql="SELECT distinct trigLabel, offlineTrgId, sum(Nevents) from ProdTriggerSet where trigSetName = '$qtrg' and prodTag = '$qprod' group by offlineTrgId ";
 
- $fileC->clear_context( );
+           $cursor =$dbh->prepare($sql)
+              || die "Cannot prepare statement: $DBI::errstr\n";
+            $cursor->execute();
 
-     foreach $tline (@artrig){
+        while(@fields = $cursor->fetchrow) {
+            my $cols=$cursor->{NUM_OF_FIELDS};
 
-     @prt = ();
-     @prt = split("::",$tline);
-     $trglable[$nlist] = @prt[0];
-     $trgid[$nlist] = @prt[1];
+            for($i=0;$i<$cols;$i++) {
 
- print <<END;
+            $trglable[$nlist] = $fields[0];
+            $trgid[$nlist] = $fields[1];
+            $nevents[$nlist] = $fields[2];
+
+	}
+
+print <<END;
 
 <TR ALIGN=CENTER HEIGHT=20 bgcolor=\"#ffdc9f\">
 <td HEIGHT=10><h3>$trglable[$nlist]</h3></td>
 <td HEIGHT=10><h3>$trgid[$nlist]</h3></td>
+<td HEIGHT=10><h3>$nevents[$nlist]</h3></td>
 </TR>
 END
-      $nlist++;
 
-    }
+	    $nlist++;
+	}
  
-   $fileC->destroy();
+   &StDbProdDisconnect();
 
  &endHtml();
 
@@ -90,13 +106,14 @@ print <<END;
 
   <html>
    <body BGCOLOR=\"cornsilk\"> 
- <h2 ALIGN=CENTER> <B>Offline trigger ID list for <font color="blue">$qtrg </font> dataset </B></h2>
+ <h2 ALIGN=CENTER> <B>Offline trigger ID list for <font color="blue">$qtrg </font> dataset <br>and  <font color="blue">$qprod </font>production</B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 >
 <TR>
-<TD ALIGN=CENTER WIDTH=\"40%\" HEIGHT=60><B><h3>Trigger lable</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"40%\" HEIGHT=60><B><h3>Offline trigger ID</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Trigger lable</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Offline trigger ID</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"30%\" HEIGHT=60><B><h3>Number of Events</h3></B></TD>
 
 </TR> 
    </head>
@@ -121,6 +138,19 @@ Last modified: $Date
 END
 
 }
+
+######################
+sub StDbProdConnect {
+    $dbh = DBI->connect("dbi:mysql:$dbname:$dbhost", $dbuser, $dbpass)
+        || die "Cannot connect to db server $DBI::errstr\n";
+}
+
+
+######################
+sub StDbProdDisconnect {
+    $dbh = $dbh->disconnect() || die "Disconnect failure $DBI::errstr\n";
+}
+
 
 ##############
 sub cgiSetup {
