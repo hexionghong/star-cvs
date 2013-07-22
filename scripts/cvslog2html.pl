ea483787e8174ef499b167cb45f920bb32a4c05f
jeromel <>
2013-07-22 16:00:14 +0000

    Odd, a grandfathered link was still in use now fixed

--- scripts/cvslog2html.pl
+++ scripts/cvslog2html.pl
@@ -14,7 +14,7 @@
 # Modified a few times from 2001-2006, J.Lauret
 #
 $commitlog   = "/afs/rhic.bnl.gov/star/packages/repository/CVSROOT/commitlog";
-$cvsHtml     = "/afs/rhic.bnl.gov/star/doc_public/www/tmp/cvs";
+$cvsHtml     = "/afs/rhic.bnl.gov/star/doc_public/www/html/tmp/cvs";
 $cvsUrl      = "/cgi-bin/protected/cvsweb.cgi";
 $currentTime = localtime time;
 $tmp         = "-tmp".getppid();
