0deab14535af156b9248a3b7304d79648c4143aa
didenko <>
2014-01-02 15:08:32 +0000

    updated for year 2014

--- cgi/gridJobsQuery.pl
+++ cgi/gridJobsQuery.pl
@@ -24,7 +24,7 @@ $dbpass="";
 $dbname="GridJobs";
 
 
- my @prodyear = ("2006","2007","2008","2009","2010","2011","2012","2013" );
+ my @prodyear = ("2006","2007","2008","2009","2010","2011","2012","2013","2014" );
 
  my $qq = new CGI;
 
@@ -61,7 +61,7 @@ print "<h3 align=center> Select year</h3>";
 print "<h4 align=center>";
 print  $qq->scrolling_list(-name=>'ryear',
                              -values=>\@prodyear,
-                             -default=>2013,
+                             -default=>2014,
 			      -size =>1);
 
 print "<p>";
