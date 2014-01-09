5ce32f6ee1e57f7577471557e31d12b2459cc402
didenko <>
2014-01-09 17:52:55 +0000

    fixes

--- cgi/fastDEVtestStatus.pl
+++ cgi/fastDEVtestStatus.pl
@@ -240,13 +240,13 @@ print <<END;
 <br>
 <TABLE ALIGN=CENTER BORDER=4 CELLSPACING=1 CELLPADDING=1 bgcolor=\"#ffdc9f\">
 <TR>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>Date</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>AutoBuild status</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>AutoBuild info</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>Test status</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>Error message</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>Test submit time</h3></B></TD>
-<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=40><B><h3>Test complete time</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Date</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>AutoBuild status</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>AutoBuild info</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Test status</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Error message</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Test submit time</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Test complete time</h3></B></TD>
 </TR>
     </body>
 END
@@ -272,8 +272,4 @@ END
 
 }
 
-##############
-sub cgiSetup {
-    $q=new CGI;
-    if ( exists($ENV{'QUERY_STRING'}) ) { print $q->header };
 }
