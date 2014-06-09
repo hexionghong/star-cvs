424c06dab1c8089df0f7d4e2f87307f2541ba293
didenko <>
2014-06-09 21:01:37 +0000

    minor updates

--- cgi/GRIDsummary.pl
+++ cgi/GRIDsummary.pl
@@ -426,7 +426,7 @@ print <<END;
       <address><a href=\"mailto:didenko\@bnl.gov\">Lidia Didenko</a></address>
 <!-- Created: March 12 2014 -->
 <!-- hhmts start -->
-Last modified: 2014-05-30
+Last modified: 2014-06-10
 <!-- hhmts end -->
   </body>
 </html>
