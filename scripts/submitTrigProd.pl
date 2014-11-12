298224580affb500b58da80164237938de828fc6
didenko <>
2014-11-12 19:10:14 +0000

    updated name of field

--- scripts/submitTrigProd.pl
+++ scripts/submitTrigProd.pl
@@ -171,9 +171,9 @@ my $prid;
 
    &create_jobs($trgsetup[$ik],$mField[$ik],$jblib[$ik],$filename[$njob],$runday[$ik],$runnum[$ik],$nevents[$ik]);
 
-   `$CRSDIR/crs_job -insert $jobFname` ; 
+#   `$CRSDIR/crs_job -insert $jobFname` ; 
 
-    `/bin/mv $jobFname $archdir`;
+#    `/bin/mv $jobFname $archdir`;
 
     print "Check jobfileNAme  ",$jobFname, "\n";      
 
@@ -186,7 +186,7 @@ my $prid;
 
   for ( my $jk = 0; $jk < $nj; $jk++ ) {
 
-  $sql="update $TrigRequestT  set dataset = '$trgsetup[$jk]', runday = '$runday[$jk]', prodtag = '$prtag[$jk]', submit = 'yes', Nfiles = '$nfiles[$jk]' where runnumber = '$runnum[$jk]' and submit = 'no' ";
+  $sql="update $TrigRequestT  set dataset = '$trgsetup[$jk]', runday = '$runday[$jk]', prodtag = '$prtag[$jk]', submit = 'yes', Nfiles_subm = '$nfiles[$jk]' where runnumber = '$runnum[$jk]' and submit = 'no' ";
 
   $rv = $dbh->do($sql) || die $dbh->errstr;
 
