1c076397892c8ba09bd5e93ff28f66201b94014d
didenko <>
2014-12-08 14:53:26 +0000

    remove optimized version

--- cgi/EVALPlots.pl
+++ cgi/EVALPlots.pl
@@ -290,10 +290,12 @@ my $graph = new GD::Graph::linespoints(650,500);
 
  } else {
 
-    @data = (\@Ndate, \@point1, \@point2 );
+#    @data = (\@Ndate, \@point1, \@point2 );
 
-    $legend[0] = "nonoptimized";
-    $legend[1] = "optimized";
+   @data = (\@Ndate, \@point1 );
+
+#    $legend[0] = "nonoptimized";
+#    $legend[1] = "optimized";
 
 
  $ylabel = $plotVal; 
