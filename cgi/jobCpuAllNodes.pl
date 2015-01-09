f1630d139c0d9c75fe6b8c219fbb5bde97424bdf
didenko <>
2015-01-09 20:32:23 +0000

    add year 2014 production job status table

--- cgi/jobCpuAllNodes.pl
+++ cgi/jobCpuAllNodes.pl
@@ -50,7 +50,7 @@ my $nowdate = $todate;
 my $thisyear = $year+1900;
 my $dyear = $thisyear - 2000;
 
-my @prodyear = ("2010","2011","2012","2013","2014");
+my @prodyear = ("2010","2011","2012","2013","2014","2015");
 my @arval = ("rtime/cpu","cpu");
 my @arnode = ("rcrs","rcas");
 my @arcrs = ();
@@ -257,33 +257,33 @@ my @cpwb = ();
     $cursor->finish();
 
 
-#  $JobStatusT = "JobStatus2014";
+  $JobStatusT = "JobStatus2014";
 
 
-#    $sql="SELECT DISTINCT prodSeries  FROM $JobStatusT ";
+    $sql="SELECT DISTINCT prodSeries  FROM $JobStatusT ";
 
-#      $cursor =$dbh->prepare($sql)
-#          || die "Cannot prepare statement: $DBI::errstr\n";
-#       $cursor->execute();
+      $cursor =$dbh->prepare($sql)
+          || die "Cannot prepare statement: $DBI::errstr\n";
+       $cursor->execute();
 
-#       while( $mpr = $cursor->fetchrow() ) {
-#          $arrprod[$npr] = $mpr;
-#          $npr++;
-#       }
-#    $cursor->finish();
+       while( $mpr = $cursor->fetchrow() ) {
+          $arrprod[$npr] = $mpr;
+          $npr++;
+       }
+    $cursor->finish();
 
 
-#    $sql="SELECT DISTINCT runDay  FROM $JobStatusT where runDay >= '2014-02-20' order by runDay" ;
+    $sql="SELECT DISTINCT runDay  FROM $JobStatusT where runDay >= '2014-02-20' order by runDay" ;
 
-#      $cursor =$dbh->prepare($sql)
-#          || die "Cannot prepare statement: $DBI::errstr\n";
-#       $cursor->execute();
+      $cursor =$dbh->prepare($sql)
+          || die "Cannot prepare statement: $DBI::errstr\n";
+       $cursor->execute();
 
-#       while( $dy = $cursor->fetchrow() ) {
-#          $ardays[$ndy] = $dy;
-#          $ndy++;
-#       }
-#    $cursor->finish();
+       while( $dy = $cursor->fetchrow() ) {
+          $ardays[$ndy] = $dy;
+          $ndy++;
+       }
+    $cursor->finish();
 
 
 
@@ -399,7 +399,7 @@ my $qnode   = $qqr->param('pnode');
  if( $qprod =~ /P13ib/ ) {$pryear = "2012"};
  if( $qprod =~ /P14ia/ ) {$pryear = "2013"};
  if( $qprod =~ /P14ig/ ) {$pryear = "2013"};
-
+ if( $qprod =~ /P14ii/ ) {$pryear = "2014"};
 
   $JobStatusT = "JobStatus".$pryear;
 
