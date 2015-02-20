f8739b2f1be10934b2e8dde03ee88f1497599782
didenko <>
2015-02-20 17:12:10 +0000

    updated for year 2015 test

--- scripts/dbLib/updateJobsQA.pl
+++ scripts/dbLib/updateJobsQA.pl
@@ -24,8 +24,8 @@ $dbpass="";
 $dbname="LibraryJobs";
 
 
-$JobStatusT = "JobStatus";
-$JobQAT = "newJobsQA";
+my $JobStatusT = "JobStatus";
+my $JobQAT = "newJobsQA";
 
 my $min;
 my $hour;
@@ -74,7 +74,7 @@ my $nowdate = ($year+1900)."-".$mon."-".$mday ;
  my $testDay;
  my $beforeDay;
  my $ik = 0;
- my $np = 4;
+ my $np = 8;
 
 
  $iday = $dayHash{$thisday};
@@ -88,17 +88,25 @@ my @nohftpath = ();
  $hftpath[0] = "/star/rcf/test/dev/daq_sl302.ittf/".$testDay."/year_2014/AuAu200_production_low_2014";
  $hftpath[1] = "/star/rcf/test/dev/daq_sl302.ittf_opt/".$testDay."/year_2014/AuAu200_production_low_2014";
 
- $hftpath[2] = "/star/rcf/test/dev/daq_sl302.ittf/".$beforeDay."/year_2014/AuAu200_production_low_2014";
- $hftpath[3] = "/star/rcf/test/dev/daq_sl302.ittf_opt/".$beforeDay."/year_2014/AuAu200_production_low_2014";
+ $hftpath[2] = "/star/rcf/test/dev/daq_sl302.ittf/".$testDay."/year_2015/production_pp200long_2015";
+ $hftpath[3] = "/star/rcf/test/dev/daq_sl302.ittf_opt/".$testDay."/year_2015/production_pp200long_2015";
+
+ $hftpath[4] = "/star/rcf/test/dev/daq_sl302.ittf/".$beforeDay."/year_2014/AuAu200_production_low_2014";
+ $hftpath[5] = "/star/rcf/test/dev/daq_sl302.ittf_opt/".$beforeDay."/year_2014/AuAu200_production_low_2014";
+
+ $hftpath[6] = "/star/rcf/test/dev/daq_sl302.ittf/".$beforeDay."/year_2015/production_pp200long_2015";
+ $hftpath[7] = "/star/rcf/test/dev/daq_sl302.ittf_opt/".$beforeDay."/year_2015/production_pp200long_2015";
 
 for ($ik = 0; $ik< $np; $ik++ ) {
 
  $nohftpath[$ik] = $hftpath[$ik];
  $nohftpath[$ik] =~ s/low/low.nohft/g;
+ $nohftpath[$ik] =~ s/long/long.nohft/g;
 
 print  $hftpath[$ik], "   % " ,$nohftpath[$ik], "\n";
 
 }
+
 
 my @avgTracks = ();
 my @avgTracksn15 = ();
@@ -120,7 +128,7 @@ my @avgRatio_Primn15 = ();
 
    &StDbLibConnect();
 
- for ($ii = 0; $ii < 2; $ii++ ){
+ for ($ii = 0; $ii < 4; $ii++ ){
 
  $sql="SELECT avg_no_tracks, avg_no_tracksnfit15, avg_no_primaryT, avg_no_primaryTnfit15, jobID FROM $JobStatusT WHERE path = '$hftpath[$ii]' and avail = 'Y' and createTime like '$nowdate%' and jobStatus = 'Done' " ;
 
@@ -143,7 +151,7 @@ my @avgRatio_Primn15 = ();
 
 #############
  
- for ($ii = 0; $ii < 2; $ii++ ){
+ for ($ii = 0; $ii < 4; $ii++ ){
 
  $sql="SELECT avg_no_tracks, avg_no_tracksnfit15, avg_no_primaryT, avg_no_primaryTnfit15 FROM $JobStatusT WHERE path = '$nohftpath[$ii]' and avail = 'Y' and createTime like '$nowdate%' and jobStatus = 'Done' " ;
 
@@ -166,7 +174,7 @@ my @avgRatio_Primn15 = ();
 
 ###############
 
- for ($ii = 2; $ii < 4; $ii++ ){
+ for ($ii = 4; $ii < 8; $ii++ ){
 
  $sql="SELECT avg_no_tracks, avg_no_tracksnfit15, avg_no_primaryT, avg_no_primaryTnfit15, jobID FROM $JobStatusT WHERE path = '$hftpath[$ii]' and avail = 'Y' and jobStatus = 'Done' " ;
 
@@ -183,12 +191,13 @@ my @avgRatio_Primn15 = ();
        $avgPrim[$ii]      = $fields[2];
        $avgPrimn15[$ii]   = $fields[3];
        $hfjobID[$ii]      = $fields[4];
+
     }
 }
 
-##########################################
+#############
  
- for ($ii = 2; $ii < 4; $ii++ ){
+ for ($ii = 4; $ii < 8; $ii++ ){
 
  $sql="SELECT avg_no_tracks, avg_no_tracksnfit15, avg_no_primaryT, avg_no_primaryTnfit15 FROM $JobStatusT WHERE path = '$nohftpath[$ii]' and avail = 'Y' and jobStatus = 'Done' " ;
 
@@ -209,7 +218,8 @@ my @avgRatio_Primn15 = ();
 
  }
 
-#########
+###############
+
 
   for ($ii = 0; $ii < $np; $ii++ ){
 
@@ -220,9 +230,12 @@ my @avgRatio_Primn15 = ();
       }else{
 	  $avgRatio_Trck[$ii] = 0;
       }
-      print "Ratio for number of tracks  ",$avgRatio_Trck[$ii], "\n";
+      }else{
+     $avgRatio_Trck[$ii] = 0;
+      }      
+      print "Ratio for number of tracks  ",$hftpath[$ii],"   ",$avgRatio_Trck[$ii], "\n";
 
-    }
+    
 #########
       if( defined $avgTracksn15[$ii] and defined $avgTracksn15_nohft[$ii] ) {
       if( $avgTracksn15_nohft[$ii] >= 0.000001 and $avgTracksn15[$ii] >= 0.000001 ) {
@@ -230,9 +243,12 @@ my @avgRatio_Primn15 = ();
       }else{
 	  $avgRatio_Trckn15[$ii] = 0;
       }
-#      print "Ratio for number of tracks  ",$avgRatio_Trckn15[$ii], "\n";
 
-    }
+      }else{
+          $avgRatio_Trckn15[$ii] = 0;
+      } 
+
+#      print "Ratio for number of tracks nfit >=15 ",$avgRatio_Trckn15[$ii], "\n";
 ########
       if( defined $avgPrim[$ii] and defined $avgPrim_nohft[$ii] ) {
       if( $avgPrim_nohft[$ii] >= 0.000001 and $avgPrim[$ii] >= 0.000001 ) {
@@ -240,9 +256,12 @@ my @avgRatio_Primn15 = ();
       }else{
 	  $avgRatio_Prim[$ii] = 0;
       }
-#      print "Ratio for number of tracks  ",$avgRatio_Prim[$ii], "\n";
-
+     }else{
+         $avgRatio_Prim[$ii] = 0;
     }
+
+#      print "Ratio for number of primary tracks  ",$avgRatio_Prim[$ii], "\n";
+
 ########
       if( defined $avgPrimn15[$ii] and defined $avgPrimn15_nohft[$ii] ) {
       if( $avgPrimn15_nohft[$ii] >= 0.000001 and $avgPrimn15[$ii] >= 0.000001 ) {
@@ -250,17 +269,17 @@ my @avgRatio_Primn15 = ();
       }else{
 	  $avgRatio_Primn15[$ii] = 0;
       }
-#      print "Ratio for number of tracks  ",$avgRatio_Primn15[$ii], "\n";
-
+     }else{
+          $avgRatio_Primn15[$ii] = 0;
      }
-
+#      print "Ratio for number of primary tracks nfit >=15  ",$avgRatio_Primn15[$ii], "\n";
 
      if( defined $avgRatio_Trck[$ii] and defined $avgRatio_Trckn15[$ii] and defined $avgRatio_Prim[$ii] and defined $avgRatio_Primn15[$ii] ) {
 
-  $sql= "update $JobQAT set avg_ratio_tracks = '$avgRatio_Trck[$ii]', avg_ratio_tracksnfit15 = '$avgRatio_Trckn15[$ii]', avg_ratio_primaryT = '$avgRatio_Prim[$ii]', avg_ratio_primaryTnfit15 = '$avgRatio_Primn15[$ii]' where path = '$hftpath[$ii]' and jobID = '$hfjobID[$ii]' and avail = 'Y' and jobStatus = 'Done' ";
+#  $sql= "update $JobQAT set avg_ratio_tracks = '$avgRatio_Trck[$ii]', avg_ratio_tracksnfit15 = '$avgRatio_Trckn15[$ii]', avg_ratio_primaryT = '$avgRatio_Prim[$ii]', avg_ratio_primaryTnfit15 = '$avgRatio_Primn15[$ii]' where path = '$hftpath[$ii]' and jobID = '$hfjobID[$ii]' and avail = 'Y' and jobStatus = 'Done' ";
 
     
-      $rv = $dbh->do($sql) || die $rv." ".$dbh->errstr;
+#     $rv = $dbh->do($sql) || die $rv." ".$dbh->errstr;
 
      }
    }
