03369dcdcc393a80283fb2a3c6a7574483e8592b
didenko <>
2015-02-26 19:28:32 +0000

    more updates

--- cgi/jobsCpuPlots.pl
+++ cgi/jobsCpuPlots.pl
@@ -368,7 +368,7 @@ END
   if( $qprod =~ /P14ia/ ) {$pryear = "2013"};
   if( $qprod =~ /P14ig/ ) {$pryear = "2013"};
   if( $qprod =~ /P14ii/ ) {$pryear = "2014"};
-
+  if( $qprod =~ /P15ib/ ) {$pryear = "2014"};
 
     $JobStatusT = "JobStatus".$pryear;
 
