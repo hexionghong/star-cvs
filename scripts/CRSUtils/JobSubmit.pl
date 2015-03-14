8a44b3fab46bfaf01186944ec4bdfca62083781e
didenko <>
2015-03-14 13:21:58 +0000

    get back to previous version because CRSQueues.pm doesn' t work

--- scripts/CRSUtils/JobSubmit.pl
+++ scripts/CRSUtils/JobSubmit.pl
@@ -77,7 +77,8 @@
 
 use lib "/afs/rhic.bnl.gov/star/packages/scripts";
 use RunDAQ;
-use CRSQueues;
+#use CRSQueues;
+use lib "/star/u/didenko/dev/scripts/CRSUtils/CRSQueues" ;
 
 
 $ThisYear = 2015;                 # Block to consider. Completely artificial
