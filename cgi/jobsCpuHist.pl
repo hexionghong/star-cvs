3af45a20ccbb2023afa202bca19ac02f8a5a2e96
didenko <>
2015-05-27 13:22:08 +0000

    adjust scale

--- cgi/jobsCpuHist.pl
+++ cgi/jobsCpuHist.pl
@@ -948,7 +948,7 @@ if($qprod eq "P14ia" ) {
  }elsif($qprod eq "P15ic" or $qprod eq "P15ie"){
      $max_y = 11200 ;
  }else{
-     $max_y = 8400 ;
+     $max_y = 11200 ;
  }
 
         $xlabel = "Job's execution time on the farm in hours";
