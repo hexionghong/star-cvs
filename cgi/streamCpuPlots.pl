41e6b760d99d2b2cf1b719af78fb7073d3541a12
didenko <>
2015-06-03 17:48:07 +0000

    more changes

--- cgi/streamCpuPlots.pl
+++ cgi/streamCpuPlots.pl
@@ -1712,7 +1712,7 @@ END
                     y_number_format => \&y_format,
 	            #labelclr => "lblack",
                     titleclr => "lblack",
-                    dclrs => [ qw(lblue lgreen lpurple orange lred lblack lyellow brown lgray lpink marine lred) ],
+                    dclrs => [ qw(lblue lgreen lpurple orange lred lblack lyellow  lgray brown lgray lpink marine lred) ],
                     line_width => 4,
                     markers => [ 2,3,4,5,6,7,8,9],
                     marker_size => 3,
