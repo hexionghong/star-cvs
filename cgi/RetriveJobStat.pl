436021876206f56e713271cd16c40fde182175fc
didenko <>
2015-12-17 16:50:37 +0000

    add checking FileCatalog MuDst entries

--- cgi/RetriveJobStat.pl
+++ cgi/RetriveJobStat.pl
@@ -213,6 +213,37 @@ my $jobname = $qtrg."%".$qprod."%";
             $nst++;
          }
 
+ }elsif($qflag eq "entryfc") {
+
+   &beginFCHtml();
+
+     $sql="SELECT jobfileName, jobStatus, NoEvents, createTime FROM $JobStatusT  where jobfileName like ? and prodSeries = ? and trigsetName = ? and jobStatus <> 'n/a' and outputFC <> 'yes'  ";
+
+      $cursor =$dbh->prepare($sql)
+          || die "Cannot prepare statement: $DBI::errstr\n";
+       $cursor->execute($jobname,$qprod,$qtrg);
+
+        while(@fields = $cursor->fetchrow) {
+            my $cols=$cursor->{NUM_OF_FIELDS};
+            $fObjAdr = \(JobAttr->new());
+
+            for($i=0;$i<$cols;$i++) {
+                my $fvalue=$fields[$i];
+                my $fname=$cursor->{NAME}->[$i];
+                # print "$fname = $fvalue\n" ;
+
+                ($$fObjAdr)->jbname($fvalue)   if( $fname eq 'jobfileName');
+                ($$fObjAdr)->jbst($fvalue)     if( $fname eq 'jobStatus');
+                ($$fObjAdr)->jbevt($fvalue)    if( $fname eq 'NoEvents'); 
+                ($$fObjAdr)->jbtrk($fvalue)    if( $fname eq 'avg_no_tracks'); 
+                ($$fObjAdr)->jbcrtime($fvalue) if( $fname eq 'createTime');               
+
+            }
+            $jbstat[$nst] = $fObjAdr;
+            $nst++;
+         }
+
+
  }elsif($qflag eq "strcpu") {
 
    &beginStrHtml();
@@ -375,6 +406,21 @@ print <<END;
 </TR>
 END
 
+}elsif($qflag eq "entryfc") {
+
+
+print <<END;
+
+<TR ALIGN=CENTER HEIGHT=10 bgcolor=\"cornsilk\">
+<td HEIGHT=10><h3>$jbfName[$nn]</h3></td>
+<td HEIGHT=10><h3>$jbStatus[$nn]</h3></td>
+<td HEIGHT=10><h3>$jbEvent[$nn]</h3></td>
+<td HEIGHT=10><h3>$jbtrack[$nn]</h3></td>
+<td HEIGHT=10><h3>$jbctime[$nn]</h3></td>
+</TR>
+END
+
+
  }elsif($qflag eq "crserr") {
 
 print <<END;
@@ -458,6 +504,28 @@ print <<END;
   <html>
    <body BGCOLOR=\"cornsilk\">
  <h2 ALIGN=CENTER> <B>List of jobs failed to transfer MuDst files on HPSS for<font color="blue"> $qprod </font> production <br> and <font color="blue">$qtrg </font> dataset  </B></h2>
+ <h3 ALIGN=CENTER> Generated on $todate</h3>
+<br>
+<TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
+<TR>
+<TD ALIGN=CENTER WIDTH=\"40%\" HEIGHT=60><B><h3>Jobfilename</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Job status</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>No.events</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"10%\" HEIGHT=60><B><h3>Avg.No.<br>tracks</h3></B></TD>
+<TD ALIGN=CENTER WIDTH=\"20%\" HEIGHT=60><B><h3>Finish time</h3></B></TD>
+</TR>
+    </body>
+END
+}
+
+#####################################
+sub beginFCHtml {
+
+print <<END;
+
+  <html>
+   <body BGCOLOR=\"cornsilk\">
+ <h2 ALIGN=CENTER> <B>List of jobs output missing in FileCatalog for<font color="blue"> $qprod </font> production <br> and <font color="blue">$qtrg </font> dataset  </B></h2>
  <h3 ALIGN=CENTER> Generated on $todate</h3>
 <br>
 <TABLE ALIGN=CENTER BORDER=5 CELLSPACING=1 CELLPADDING=2 bgcolor=\"#ffdc9f\">
