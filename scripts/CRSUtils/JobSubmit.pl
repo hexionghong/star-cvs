720fbf73a51dbec4638111cd152e4e9e8a098b40
didenko <>
2016-02-07 21:18:13 +0000

    set 2016 year

--- scripts/CRSUtils/JobSubmit.pl
+++ scripts/CRSUtils/JobSubmit.pl
@@ -80,7 +80,7 @@ use RunDAQ;
 use CRSQueues;
 
 
-$ThisYear = 2015;                 # Block to consider. Completely artificial
+$ThisYear = 2016;                 # Block to consider. Completely artificial
                                   # and used to preserve older options in if
                                   # block along with current option.
 $HPSS     = 1;                    # turn to 0 for UNIX staging only
