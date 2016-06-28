22bee5fc763b64183e1a5a7816f77819eef18d89
didenko <>
2016-06-28 19:20:34 +0000

    add dAu datasample to new library plots

--- cgi/NewLibPlots.pl
+++ cgi/NewLibPlots.pl
@@ -32,6 +32,9 @@ my @data = ();
 my @legend = ();
 
 my @prod_set = (
+                "daq_ittf/year_2016/dAu200_production_2016",
+                "daq_ittf/year_2016/dAu20_production_2016",
+                "daq_ittf/year_2016/dAu39_production_2016",
                 "daq_ittf/year_2016/AuAu200_production_2016",
                 "daq_ittf/year_2015/production_pAu200_2015",
                 "daq_ittf/year_2015/production_pAl200_2015",
